7a3fd58ab780fd4664511420af169e40f5800d0c
arieum <lar713@naver.com>
2024-06-24 15:34:19 +0900

    Add functionality to show additional fields on '더보기' Textview click

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,19 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.detail_button).setOnClickListener{
+            it.visibility = View.GONE
+            findViewById<LinearLayout>(R.id.detail_form_area).visibility = View.VISIBLE
+        }
     }
 }
