d9d0ef01290a7fe0c7f1667834c621a86f50dc88
Kjamm <happyminig@gmail.com>
2024-06-25 15:21:30 +0900

    feat : 전화번호 미입력시 토스트 메세지 표시 기능 추가

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -9,17 +9,22 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     private lateinit var nameEditText : EditText
     private lateinit var saveButton : Button
+    private lateinit var phoneNumberEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         nameEditText = findViewById(R.id.name)
         saveButton = findViewById(R.id.save)
+        phoneNumberEditText = findViewById(R.id.phoneNumber)
 
         saveButton.setOnClickListener {
             when {
                 nameEditText.text.toString().isEmpty() -> {
                     Toast.makeText(this, "이름은 필수 값 입니다.", Toast.LENGTH_SHORT).show()
+                }
+                phoneNumberEditText.text.toString().isEmpty() -> {
+                    Toast.makeText(this, "전화번호는 필수 값 입니다.", Toast.LENGTH_SHORT).show()
                 }
             }
         }
