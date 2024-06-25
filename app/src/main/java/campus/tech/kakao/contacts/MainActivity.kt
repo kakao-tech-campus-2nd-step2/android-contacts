57f1e73325fbcb47a21a419a7f4daed1dbe279fc
changukLee <ckddnr5527@gmail.com>
2024-06-25 14:51:40 +0900

    feat: update See More Feature
    더보기를 누르면 나머지 입력폼들이 보여지는 기능 추가

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,16 +1,24 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
 
 class MainActivity : AppCompatActivity() {
     lateinit var nameEditText: EditText
     lateinit var phoneNumEditText: EditText
+    lateinit var emailEditText: EditText
+    lateinit var birthdayEditText: EditText
+    lateinit var genderTextView: TextView
+    lateinit var memoEditText: EditText
     lateinit var cancelBtn: Button
     lateinit var saveBtn: Button
+    lateinit var seeMoreLayout:LinearLayoutCompat
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,12 +30,18 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() {
         nameEditText = findViewById(R.id.name_edit_text)
         phoneNumEditText = findViewById(R.id.phone_num_edit_text)
+        emailEditText = findViewById(R.id.email_edit_text)
+        birthdayEditText = findViewById(R.id.birthday_edit_text)
+        genderTextView = findViewById(R.id.gender_text_view)
+        memoEditText = findViewById(R.id.memo_edit_text)
         cancelBtn = findViewById(R.id.cancel_btn)
         saveBtn = findViewById(R.id.save_btn)
+        seeMoreLayout = findViewById(R.id.see_more_layout)
     }
 
     private fun setOnClickListeners() {
         setOnClickListenerOfSaveBtn()
+        setOnClickListenerOfSeeMoreLayout()
     }
 
     private fun setOnClickListenerOfSaveBtn() {
@@ -37,6 +51,15 @@ class MainActivity : AppCompatActivity() {
             } else if (phoneNumEditText.text.isEmpty()) {
                 Toast.makeText(this, "전화 번호는 필수 값 입니다.", Toast.LENGTH_LONG).show()
             }
+        }
+    }
+
+    private fun setOnClickListenerOfSeeMoreLayout() {
+        seeMoreLayout.setOnClickListener {
+            seeMoreLayout.visibility = View.GONE
+            birthdayEditText.visibility = View.VISIBLE
+            genderTextView.visibility = View.VISIBLE
+            memoEditText.visibility = View.VISIBLE
         }
     }
 }
