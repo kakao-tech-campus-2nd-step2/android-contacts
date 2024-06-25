e1480962737f1cbdcf9370ef054066f280b3139b
KSC <ksc1008@naver.com>
2024-06-25 15:22:32 +0900

    feat: added more options to the layout
    added append option button
    added birthday, gender, memo input field
    
    in initial state, additional options layout are set to view.GONE
    added click listener to append options button to make it visible

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,27 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addcontact)
+        setMoreOptionsListener()
+    }
+
+    fun setMoreOptionsListener(){
+        findViewById<View>(R.id.more_options).setOnClickListener {
+            appendOptions()
+        }
+    }
+
+    fun appendOptions(){
+        findViewById<View>(R.id.more_options).visibility = View.GONE
+        findViewById<View>(R.id.additional_inputs).visibility = View.VISIBLE
     }
 }
