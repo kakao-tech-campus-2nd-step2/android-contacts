953526d0c4a43cab230d466fbe06b05f43f6a849
sun <50648835+kimseongyu@users.noreply.github.com>
2024-06-26 02:54:54 +0900

    feature: set up functions call when event occurs

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,32 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val moreBtn = findViewById<TextView>(R.id.more)
+        val moreForm = findViewById<ConstraintLayout>(R.id.more_form)
+        val cancel = findViewById<TextView>(R.id.cancel)
+        val save = findViewById<TextView>(R.id.save)
+
+        moreBtn.setOnClickListener {
+            swapVisibility(moreBtn, moreForm)
+        }
+        cancel.setOnClickListener {
+            clickCancelEvent()
+        }
+        save.setOnClickListener {
+            RequiredInputVerify()
+        }
+    }
     }
 }
