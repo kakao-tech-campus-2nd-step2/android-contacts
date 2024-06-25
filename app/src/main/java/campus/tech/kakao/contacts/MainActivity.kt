d8a6c4118f1632b0488e4242f69ba81a4b5b4145
KSC <ksc1008@naver.com>
2024-06-25 15:40:01 +0900

    feat: added input validation
    if name, phoneNumber input fields are empty, shows toast message
    email address can be empty but if not, the format must be complete. Otherwise, show toast message
    
    only if all validation are passed, show completion toast message to user

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -3,17 +3,36 @@ package campus.tech.kakao.contacts
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var emailInput:EditText
+    lateinit var nameInput:EditText
+    lateinit var phoneInput:EditText
+    lateinit var birthdayInput:EditText
+    var gender:Int = -1
+    lateinit var memoInput:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addcontact)
+        initiateInputFields()
         setMoreOptionsListener()
         setButtonsListener()
+    }
+
+    fun initiateInputFields(){
+        nameInput = findViewById(R.id.input_name)
+        emailInput = findViewById(R.id.input_mail)
+        phoneInput = findViewById(R.id.input_tel)
+        birthdayInput = findViewById(R.id.input_birthday)
+        memoInput = findViewById(R.id.input_memo)
+        gender = findViewById<RadioGroup>(R.id.input_gender).checkedRadioButtonId
     }
 
     fun setMoreOptionsListener(){
@@ -24,11 +43,48 @@ class MainActivity : AppCompatActivity() {
 
     fun setButtonsListener(){
         findViewById<Button>(R.id.button_submit).setOnClickListener {
-            saveSuccess()
+            if(validateInputs()){
+                saveSuccess()
+            }
         }
         findViewById<Button>(R.id.button_cancel).setOnClickListener {
             cancel()
         }
+    }
+
+    fun validateInputs():Boolean{
+        if(checkNameEmpty()){
+            showToast("이름 값은 필수입니다")
+            return false
+        }
+        if(checkPhoneNumberEmpty()){
+            showToast("전화번호 값은 필수입니다")
+            return false
+        }
+        if(!checkEmailEmpty()){
+            if(!verifyEmail(emailInput.text.toString())){
+                showToast("잘못된 이메일 형식입니다")
+                return false
+            }
+        }
+        return true
+    }
+
+    fun checkNameEmpty():Boolean{
+        return nameInput.text.isEmpty()
+    }
+
+    fun checkPhoneNumberEmpty():Boolean{
+        return phoneInput.text.isEmpty()
+    }
+
+    fun checkEmailEmpty():Boolean{
+        return emailInput.text.isEmpty()
+    }
+
+    fun verifyEmail(emailText:String):Boolean{
+        val emailVerifyingRegex = Regex("^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+\$")
+        return emailVerifyingRegex.matches(emailText)
     }
 
     fun appendOptions(){
