38f158a62ca537c7313c9b0f73d701eef90523cd
fiveJinw <fiveJinw1124@gmail.com>
2024-06-25 15:49:57 +0900

    feat : Add feature that display Toast Message when cancel button is clicked

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,28 @@
 package campus.tech.kakao.contacts
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cancelButtonSetOnClickListner(this)
     }
+
+    fun cancelButtonSetOnClickListner(context : Context){
+        findViewById<TextView>(R.id.button_cancel).setOnClickListener(){
+            displayCancelMessage(context)
+        }
+    }
+
+    fun displayCancelMessage(context: Context){
+        val cancelMessage = Toast.makeText(context, "취소 되었습니다", Toast.LENGTH_SHORT)
+        cancelMessage.show()
+    }
+
 }
+
