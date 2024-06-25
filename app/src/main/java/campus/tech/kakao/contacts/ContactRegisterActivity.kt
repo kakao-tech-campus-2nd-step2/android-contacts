037366f536bc8b1a33f3639ca7bc14b33c0a573a
Jeong Suhyeon <jsh00325@naver.com>
2024-06-25 20:28:07 +0900

    feat: Add Contact Input Layout
    - 연락처의 정보를 입력할 수 있는 레이아웃 제작

---
+++ app/src/main/java/campus/tech/kakao/contacts/ContactRegisterActivity.kt
@@ -0,0 +1,16 @@
+package campus.tech.kakao.contacts
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ContactRegisterActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_contact_register)
+
+
+	}
+}
