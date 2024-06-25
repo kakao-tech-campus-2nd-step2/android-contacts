51e52603c2a013725005d6ee3e24bbdb88d9ce2f
Kjamm <happyminig@gmail.com>
2024-06-25 15:15:14 +0900

    feat : 이름 입력 기능 및 이름 미입력시 토스트 메세지 표시 기능 추가

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,27 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nameEditText : EditText
+    private lateinit var saveButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameEditText = findViewById(R.id.name)
+        saveButton = findViewById(R.id.save)
+
+        saveButton.setOnClickListener {
+            when {
+                nameEditText.text.toString().isEmpty() -> {
+                    Toast.makeText(this, "이름은 필수 값 입니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
