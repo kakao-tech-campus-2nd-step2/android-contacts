40a4e1ccfaa191b90b83d1679636a0696f42d8d2
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-06-26 02:27:09 +0900

    feat: showDetail on contact editText
    연락처 정보 입력란의 더보기 기능 추가
    
    더보기 클릭 시 입력란의 LinearLayout height를 변경한다.
    더보기 뷰는 INVISIBLE 처리함

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,23 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editTextList = findViewById<LinearLayout>(R.id.editTextList)
+        val showMore = findViewById<LinearLayout>(R.id.more)
+
+        showMore.setOnClickListener {
+            var layoutParams = editTextList.layoutParams
+            layoutParams.height = R.dimen.contact_list_height_detail
+            editTextList.layoutParams = layoutParams
+            showMore.visibility = INVISIBLE
+        }
     }
 }
