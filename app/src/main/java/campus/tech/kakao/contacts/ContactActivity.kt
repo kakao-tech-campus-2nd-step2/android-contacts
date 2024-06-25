b45ae19b70b4cda94441245639092c7c71aad0af
aengzu <jeon7879@naver.com>
2024-06-26 01:16:34 +0900

    feat(activity): connect views with findViewById - Added moreButton setOnClickListener - Added findViewById

--- app/src/main/java/campus/tech/kakao/contacts/ContactActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactActivity.kt
@@ -1,14 +1,35 @@
 package campus.tech.kakao.contacts
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+        val phoneNumEditText = findViewById<EditText>(R.id.phoneNumEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val birthEditText = findViewById<EditText>(R.id.birthdayEditText)
+        val moreButton = findViewById<LinearLayout>(R.id.moreButton)
+        val memoEditText = findViewById<EditText>(R.id.memoEditText)
+        val genderGroup = findViewById<RadioGroup>(R.id.genderRadioGroup)
+        val saveButton = findViewById<TextView>(R.id.saveButton)
+        val cancelButton = findViewById<TextView>(R.id.cancelButton)
+
+        moreButton.setOnClickListener {
+            memoEditText.visibility = TextView.VISIBLE
+            birthEditText.visibility = TextView.VISIBLE
+            genderGroup.visibility = TextView.VISIBLE
+            moreButton.visibility = TextView.GONE
+        }
         }
     }
--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,14 @@
 package campus.tech.kakao.contacts
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact)
+        val intent = Intent(this, ContactActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
