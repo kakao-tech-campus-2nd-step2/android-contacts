3f3f5539c650d0e546af68525f6e276ca5d85b51
KSC <ksc1008@naver.com>
2024-06-25 15:27:27 +0900

    feat: added onClickListener to submit, cancel button
    there is no functionality to check feasibility yet.
    when buttons clicks, show message with Toast.

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -2,8 +2,9 @@ package campus.tech.kakao.contacts
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addcontact)
         setMoreOptionsListener()
+        setButtonsListener()
     }
 
     fun setMoreOptionsListener(){
@@ -20,8 +22,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun setButtonsListener(){
+        findViewById<Button>(R.id.button_submit).setOnClickListener {
+            saveSuccess()
+        }
+        findViewById<Button>(R.id.button_cancel).setOnClickListener {
+            cancel()
+        }
+    }
+
     fun appendOptions(){
         findViewById<View>(R.id.more_options).visibility = View.GONE
         findViewById<View>(R.id.additional_inputs).visibility = View.VISIBLE
+    }
+
+    fun saveSuccess(){
+        showToast("저장이 완료 되었습니다")
+    }
+
+    fun cancel(){
+        showToast("취소 되었습니다")
+    }
+
+    fun showToast(message:String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
