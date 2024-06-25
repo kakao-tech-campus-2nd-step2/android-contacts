74f056ee6c348a08c8b5232df1aec1d2d9a1ff44
sumin <cleonn7878@gmail.com>
2024-06-25 15:47:58 +0900

    feat: 이름, 전화번호가 입력되지 않으면 저장하지 않는 기능 구현

--- app/src/main/java/campus/tech/kakao/contacts/ContactActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactActivity.kt
@@ -3,6 +3,7 @@ package campus.tech.kakao.contacts
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +17,10 @@ class ContactActivity : AppCompatActivity() {
         val cancelButton: Button = findViewById(R.id.cancel)
         val saveButton: Button = findViewById(R.id.save)
 
+        val name: EditText = findViewById(R.id.name)
+        val phoneNumber: EditText = findViewById(R.id.phone_number)
+        val mail: EditText = findViewById(R.id.mail)
+
         cancelButton.setOnClickListener {
             Toast.makeText(this, "취소 되었습니다", Toast.LENGTH_SHORT).show()
             val returnIntent: Intent = Intent()
@@ -24,10 +29,35 @@ class ContactActivity : AppCompatActivity() {
         }
 
         saveButton.setOnClickListener {
-            Toast.makeText(this, "저장이 완료 되었습니다", Toast.LENGTH_SHORT).show()
-            val returnIntent: Intent = Intent()
-            setResult(RESULT_OK, returnIntent)
-            finish()
+            if (isValidInfo(name, phoneNumber)) {
+                Toast.makeText(this, "저장이 완료 되었습니다", Toast.LENGTH_SHORT).show()
+                val returnIntent: Intent = Intent()
+                setResult(RESULT_OK, returnIntent)
+                finish()
+            }
         }
+    }
+
+    fun isValidInfo(name: EditText, phoneNumber: EditText): Boolean {
+        return if (!isValidName(name)) {
+            false
+        } else if (!isValidNumber(phoneNumber)) {
+            false
+        } else true
+    }
+
+    private fun isValidName(name: EditText): Boolean {
+        return if (name.text.toString() == "") {
+            Toast.makeText(this, "이름은 반드시 입력해야 합니다", Toast.LENGTH_SHORT).show()
+            false
+        } else true
+    }
+
+    private fun isValidNumber(phoneNumber: EditText): Boolean {
+        val v : Regex = Regex("^[0-9]$")
+        return if (phoneNumber.text.toString() == "") {
+            Toast.makeText(this, "전화번호는 반드시 입력해야 합니다", Toast.LENGTH_SHORT).show()
+            false
+        } else true
     }
 }
