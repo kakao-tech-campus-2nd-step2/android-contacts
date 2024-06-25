9cca3c55d46b59a0d515d6bacca6db422cf09253
Jeong Suhyeon <jsh00325@naver.com>
2024-06-25 20:56:48 +0900

    feat: Add a Feature for Cancel Button
    - clearInputFields()를 통해 입력 값들 초기화
    - 취소되었음을 Toast를 통해서 알림

--- app/src/main/java/campus/tech/kakao/contacts/ContactRegisterActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactRegisterActivity.kt
@@ -2,18 +2,43 @@ package campus.tech.kakao.contacts
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ContactRegisterActivity : AppCompatActivity() {
+	lateinit var nameInputView: EditText
+	lateinit var phoneInputView: EditText
+	lateinit var emailInputView: EditText
+	lateinit var birthdayInputView: EditText
+	lateinit var femaleRadioButton: RadioButton
+	lateinit var maleRadioButton: RadioButton
+	lateinit var memoInputView: EditText
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_contact_register)
 
 		setMoreInfoVisibility()
+
+		nameInputView = findViewById(R.id.contactName)
+		phoneInputView = findViewById(R.id.contactPhone)
+		emailInputView = findViewById(R.id.contactMail)
+		birthdayInputView = findViewById(R.id.contactBirthday)
+		femaleRadioButton = findViewById(R.id.femaleRadioButton)
+		maleRadioButton = findViewById(R.id.maleRadioButton)
+		memoInputView = findViewById(R.id.contactMemo)
+
+		findViewById<Button>(R.id.cancelButton).setOnClickListener {
+			clearInputFields()
+			Toast.makeText(this@ContactRegisterActivity, "취소되었습니다.", Toast.LENGTH_SHORT).show()
+		}
 	}
 
 	private fun setMoreInfoVisibility() {
@@ -21,5 +46,15 @@ class ContactRegisterActivity : AppCompatActivity() {
 			findViewById<LinearLayout>(R.id.moreInfoLayout).visibility = View.VISIBLE
 			moreInfoButton.visibility = View.GONE
 		}
+	}
+
+	private fun clearInputFields() {
+		nameInputView.text.clear()
+		phoneInputView.text.clear()
+		emailInputView.text.clear()
+		birthdayInputView.text.clear()
+		femaleRadioButton.isChecked = false
+		maleRadioButton.isChecked = false
+		memoInputView.text.clear()
 	}
 }
