4cb00caba8e91248cd7d3d7d6aaddbe52604c99e
fiveJinw <fiveJinw1124@gmail.com>
2024-06-25 20:09:10 +0900

    feat : Add feature that display toast message if name, number field is not fill

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -2,14 +2,22 @@ package campus.tech.kakao.contacts
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name : EditText
+    lateinit var phoneNumber : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        name = findViewById<EditText>(R.id.input_name)
+        phoneNumber = findViewById<EditText>(R.id.input_phone_number)
         saveButtonSetOnClickListner(this)
         cancelButtonSetOnClickListner(this)
     }
@@ -32,8 +40,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun displaySaveMessage(context: Context){
-        val saveMessage = Toast.makeText(context, "저장이 완료 되었습니다.", Toast.LENGTH_SHORT)
+        Log.d("phone", "" + name.text)
+        val text = checkNameAndPhoneNum()
+        val saveMessage = Toast.makeText(context, text, Toast.LENGTH_SHORT)
         saveMessage.show()
+    }
+
+    fun checkNameAndPhoneNum() : String{
+        if (name.text.toString() == ""){
+            name.requestFocus()
+            return "이름을 입력해야만 합니다"
+        }
+        else if(phoneNumber.text.toString() == ""){
+            phoneNumber.requestFocus()
+            return "번호를 입력해야만 합니다"
+        }
+        else{
+            return "저장이 완료 되었습니다"
+        }
     }
 }
 
