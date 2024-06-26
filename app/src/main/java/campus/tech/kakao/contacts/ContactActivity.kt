f2056ad88bd69cd5ae2fa7f1b057f193eb25f652
aengzu <jeon7879@naver.com>
2024-06-26 10:18:56 +0900

    feat: show DatePickerDialog on birth view click

--- app/src/main/java/campus/tech/kakao/contacts/ContactActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactActivity.kt
@@ -1,8 +1,10 @@
 package campus.tech.kakao.contacts
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.RadioGroup
@@ -11,25 +13,48 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class ContactActivity : AppCompatActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var phoneNumEditText: EditText
+    private lateinit var genderRadioGroup: RadioGroup
+    private lateinit var birthEditText: EditText
+    private lateinit var memoEditText: EditText
+    private lateinit var saveButton: TextView
+    private lateinit var cancelButton: TextView
+    private lateinit var moreButton: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
 
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val phoneNumEditText = findViewById<EditText>(R.id.phoneNumEditText)
-        val emailEditText = findViewById<EditText>(R.id.emailEditText)
-        val birthEditText = findViewById<EditText>(R.id.birthdayEditText)
-        val moreButton = findViewById<LinearLayout>(R.id.moreButton)
-        val memoEditText = findViewById<EditText>(R.id.memoEditText)
-        val genderGroup = findViewById<RadioGroup>(R.id.genderRadioGroup)
-        val saveButton = findViewById<TextView>(R.id.saveButton)
-        val cancelButton = findViewById<TextView>(R.id.cancelButton)
+        nameEditText = findViewById<EditText>(R.id.nameEditText)
+        phoneNumEditText = findViewById<EditText>(R.id.phoneNumEditText)
+        emailEditText = findViewById<EditText>(R.id.emailEditText)
+        birthEditText = findViewById<EditText>(R.id.birthdayEditText)
+        moreButton = findViewById<LinearLayout>(R.id.moreButton)
+        memoEditText = findViewById<EditText>(R.id.memoEditText)
+        genderRadioGroup = findViewById<RadioGroup>(R.id.genderRadioGroup)
+        saveButton = findViewById<TextView>(R.id.saveButton)
+        cancelButton = findViewById<TextView>(R.id.cancelButton)
 
         moreButton.setOnClickListener {
             memoEditText.visibility = TextView.VISIBLE
             birthEditText.visibility = TextView.VISIBLE
-            genderGroup.visibility = TextView.VISIBLE
+            genderRadioGroup.visibility = TextView.VISIBLE
             moreButton.visibility = TextView.GONE
         }
+
+        birthEditText.setOnClickListener {
+            showDatePickerDialog()
         }
+
+
+        }
+    private fun showDatePickerDialog(){
+        val datePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            birthEditText.setText("$year-${month + 1}-$dayOfMonth")
+        }, 2021, 0, 1)
+        datePickerDialog.show()
     }
+
+}
