dc5f668db8dbbc4f35f7896f521d23954b8fb672
Jeong hoon <hoondb@naver.com>
2024-06-26 20:03:41 +0900

    feat: Add feature to move to input form activity

--- app/src/main/java/campus/tech/kakao/contacts/ContactListActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactListActivity.kt
@@ -1,6 +1,8 @@
 package campus.tech.kakao.contacts
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class ContactListActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnAdd = findViewById<ImageView>(R.id.btnAdd)
+
+        btnAdd.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
