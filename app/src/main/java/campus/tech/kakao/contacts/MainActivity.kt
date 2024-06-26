b16ea1b044dbe88f083daed777257cebe0ed7dec
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-06-26 15:00:16 +0900

    refactor: separte ui method
    뷰 확장시의 메서드 분리 및 일반화

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -6,6 +6,18 @@ import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    fun extendEditTextList(view : View, sizeId : Int){
+        var layoutParams = view.layoutParams
+        layoutParams.height = resources.getDimensionPixelSize(sizeId)
+        view.layoutParams = layoutParams
+    }
+
+    fun toggleViewVisibility(view : View){
+        view.visibility = when(view.visibility){
+            GONE -> VISIBLE
+            else -> GONE
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -13,11 +25,9 @@ class MainActivity : AppCompatActivity() {
         val editTextList = findViewById<LinearLayout>(R.id.editTextList)
         val showMore = findViewById<LinearLayout>(R.id.more)
 
-        showMore.setOnClickListener {
-            var layoutParams = editTextList.layoutParams
-            layoutParams.height = R.dimen.contact_list_height_detail
-            editTextList.layoutParams = layoutParams
-            showMore.visibility = INVISIBLE
+        showDetail.setOnClickListener {
+            extendEditTextList(editTextList,R.dimen.contact_list_height_detail)
+            toggleViewVisibility(showDetail)
         }
     }
 }
