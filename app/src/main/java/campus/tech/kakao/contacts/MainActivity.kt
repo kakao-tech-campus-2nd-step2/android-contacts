e027741a4765525df50b3932f04cc0909fd4b631
이지은 <jieun7056@naver.com>
2024-06-26 14:51:31 +0900

    feat: 더보기 클릭하면 입력 폼 확장

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,49 @@
 package campus.tech.kakao.contacts
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name_input: EditText = findViewById(R.id.name_input)
+        val phone_number_input: EditText = findViewById(R.id.phone_number_input)
+        val email_input: EditText = findViewById(R.id.email_input)
+        val birth_date_input: EditText = findViewById(R.id.birth_date_input)
+        val gender_input: EditText = findViewById(R.id.gender_input)
+        val memo_input: EditText = findViewById(R.id.memo_input)
+
+        val save_button: Button = findViewById(R.id.save_button)
+        val cancel_button: Button = findViewById(R.id.cancel_button)
+        val see_more_button: LinearLayoutCompat = findViewById(R.id.see_more_button)
+        val see_more_input_form: LinearLayoutCompat = findViewById(R.id.see_more_input_form)
+
+        see_more_button.setOnClickListener{
+            see_more_button.visibility = View.GONE
+            see_more_input_form.visibility = View.VISIBLE
+        }
+
+
+        save_button.setOnClickListener {
+            if (name_input.text == null) {
+                Toast.makeText(this, "이름을 입력해주세요.", Toast.LENGTH_SHORT)
+            }
+            else if (phone_number_input.text == null) {
+                Toast.makeText(this, "전화번호를 입력해주세요.", Toast.LENGTH_SHORT)
+            } else {
+
+            }
+        }
+
+
     }
 }
