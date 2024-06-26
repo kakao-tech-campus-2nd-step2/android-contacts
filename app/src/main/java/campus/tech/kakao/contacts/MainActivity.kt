7aed137c41cf150e6421260cdb16909d510a204f
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-06-26 15:02:38 +0900

    feat: func 3 - input field base

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -2,10 +2,36 @@ package campus.tech.kakao.contacts
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog //날짜 선택
+import java.text.SimpleDateFormat // 날짜 형식 지정
+import java.util.*
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nameText = findViewById<EditText>(R.id.nameText)
+        val phoneText = findViewById<EditText>(R.id.phoneText)
+        val emailText = findViewById<EditText>(R.id.emailText)
+        val birthText = findViewById<TextView>(R.id.birthText)
+        val memoText = findViewById<EditText>(R.id.memoText)
+        val moreLayout = findViewById<LinearLayout>(R.id.moreLayout)
+        val moreText = findViewById<TextView>(R.id.moreText)
+        val cancelButton = findViewById<Button>(R.id.cancelButton)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val genderRadio = findViewById<RadioGroup>(R.id.genderRadio)
+        val femaleButton = findViewById<RadioButton>(R.id.femaleButton)
+        val maleButton = findViewById<RadioButton>(R.id.maleButton)
+
+        //호출
+        moreText.setOnClickListener{
+            toggleMore()
+        }
+
+        birthText.setOnClickListener{
+            showDatePicker()
+        }
     }
 }
