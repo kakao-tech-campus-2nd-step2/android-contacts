b4bdadf4407ea95a51641907c38efc1eea2c688c
changukLee <ckddnr5527@gmail.com>
2024-06-26 14:56:51 +0900

    feat: update Activity Transition feature
    + 버튼을 누르면 연락처 작성 액티비티로 넘어가도록 구현

--- app/src/main/java/campus/tech/kakao/contacts/RegisterActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/RegisterActivity.kt
@@ -1,16 +1,33 @@
 package campus.tech.kakao.contacts
 
-
+import android.content.Intent
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var registerBtnLayout:FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        initViews()
+        setOnClickListeners()
     }
 
+    private fun initViews() {
+        registerBtnLayout = findViewById(R.id.register_btn_layout)
+    }
 
+    private fun setOnClickListeners() {
+        setOnClickListenerOfRegisterBtnLayout()
+    }
+
+    private fun setOnClickListenerOfRegisterBtnLayout() {
+        registerBtnLayout.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
+        }
+    }
 
 }
