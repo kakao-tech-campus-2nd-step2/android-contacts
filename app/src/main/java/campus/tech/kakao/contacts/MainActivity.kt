5010f24f8bab73ef01441e361c980a3c11184b61
jooiss <jusong513@pusan.ac.kr>
2024-06-27 17:05:03 +0900

    feat: Add name input form layout

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,13 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
