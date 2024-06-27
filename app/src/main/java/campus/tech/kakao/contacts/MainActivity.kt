8a13cd0831a1230d3738b867c7546dd58561892f
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2024-06-27 16:34:45 +0900

    feat: Implement birthday input form with DatePickerDialog in MainActivity Added birthday input functionality using DatePickerDialog in MainActivity.
    - Integrated a DatePickerDialog to allow users to select a birthdate.
    - Implemented logic in MainActivity to capture and process selected date information.

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,9 +1,11 @@
 package campus.tech.kakao.contacts
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         val cancelButton: Button = findViewById(R.id.cancel_button)
         val moreButton: LinearLayout = findViewById(R.id.more_button)
         val additionalFields: LinearLayout = findViewById(R.id.additional_fields)
+        val birthdayField: EditText = findViewById(R.id.birthday_field)
 
 
         moreButton.setOnClickListener {
@@ -29,6 +32,7 @@ class MainActivity : AppCompatActivity() {
             val name = nameField.text.toString()
             val phone = phoneField.text.toString()
             val email = emailField.text.toString()
+            val birthday = birthdayField.text.toString()
 
             if (name.isEmpty()) {
                 Toast.makeText(this, getString(R.string.toast_name_is_essential), Toast.LENGTH_SHORT).show()
@@ -36,12 +40,27 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this,
                     getString(R.string.toast_phone_number_is_essential), Toast.LENGTH_SHORT).show()
             } else {
-                var message = getString(R.string.toast_saved)
+                val message = getString(R.string.toast_saved)
                 Toast.makeText(this, message, Toast.LENGTH_LONG).show()
             }
         }
         cancelButton.setOnClickListener {
             Toast.makeText(this, getString(R.string.toast_canceled), Toast.LENGTH_SHORT).show()
         }
+
+        birthdayField.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "$selectedYear-${selectedMonth + 1}-$selectedDay"
+                birthdayField.setText(selectedDate)
+            }, year, month, day)
+
+            datePickerDialog.show()
+        }
+
     }
 }
