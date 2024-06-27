346215c4b1a385e24f04ab113f47fc4d44a2d675
SHEEN <normengdie@pusan.ac.kr>
2024-06-28 01:45:30 +0900

    feat : add findViewByIds

--- app/src/main/java/campus/tech/kakao/contacts/SpecificInformationActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/SpecificInformationActivity.kt
@@ -1,11 +1,18 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SpecificInformationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.specific_information)
+        val textViewName = findViewById<TextView>(R.id.textViewName)
+        val textViewPhoneNumber = findViewById<TextView>(R.id.textViewPhoneNumber)
+        val textViewEmail = findViewById<TextView>(R.id.textViewEmail)
+        val textViewBirth = findViewById<TextView>(R.id.textViewBirth)
+        val textViewGender = findViewById<TextView>(R.id.textViewGender)
+        val textViewMemo = findViewById<TextView>(R.id.textViewMemo)
     }
 }
