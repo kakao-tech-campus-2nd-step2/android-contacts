64f13b14e5e0a75b7f510928c6a2dfc149ea1d72
SHEEN <normengdie@pusan.ac.kr>
2024-06-27 21:46:55 +0900

    feat : created savemessage fun

--- app/src/main/java/campus/tech/kakao/contacts/AddInfor.kt
+++ app/src/main/java/campus/tech/kakao/contacts/AddInfor.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class AddInfor : AppCompatActivity() {
@@ -19,4 +20,7 @@ class AddInfor : AppCompatActivity() {
         val inputName = findViewById<EditText>(R.id.inputName)
         val inputPhoneNumber = findViewById<EditText>(R.id.inputPhoneNumber)
     }
+}
+private fun savemessage(saveButton: Button) {
+    Toast.makeText(saveButton.context, "저장이 완료 되었습니다.", Toast.LENGTH_SHORT).show()
 }
