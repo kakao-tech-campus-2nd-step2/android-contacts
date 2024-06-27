e58590be1c1ea9517d86bb077d4f9bedb1967bbe
khyeonm <122lamb@naver.com>
2024-06-27 19:23:38 +0900

    feat: implement simple input function

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,45 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name = findViewById<EditText>(R.id.name)
+        val phone = findViewById<EditText>(R.id.phone)
+        val mail = findViewById<EditText>(R.id.mail)
+        val save = findViewById<Button>(R.id.save)
+        val cancel = findViewById<Button>(R.id.cancel)
+        val more = findViewById<LinearLayoutCompat>(R.id.more)
+
+        name.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        phone.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        mail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        cancel.setOnClickListener {
+            Toast.makeText(this@MainActivity, "취소 되었습니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
