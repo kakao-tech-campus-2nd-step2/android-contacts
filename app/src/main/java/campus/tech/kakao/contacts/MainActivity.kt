4e6200f79bf317d7c077371f4e2fab93dac2370c
sun <50648835+kimseongyu@users.noreply.github.com>
2024-06-28 04:20:24 +0900

    feat: add onCreate's function call
    previous mainActivity is moved to add contact

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,56 +1,36 @@
 package campus.tech.kakao.contacts
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.EditText
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    val contacts = mutableListOf<MutableList<String>>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val moreBtn = findViewById<TextView>(R.id.more)
-        val moreForm = findViewById<ConstraintLayout>(R.id.more_form)
-        val cancel = findViewById<TextView>(R.id.cancel)
-        val save = findViewById<TextView>(R.id.save)
+        val gotoAddContactBtn = findViewById<Button>(R.id.goto_add_contact)
 
-        moreBtn.setOnClickListener {
-            swapVisibility(moreBtn, moreForm)
-        }
-        cancel.setOnClickListener {
-            clickCancelEvent()
-        }
-        save.setOnClickListener {
-            RequiredInputVerify()
+        gotoAddContactBtn.setOnClickListener {
+            gotoAddContact()
         }
     }
 
-    fun swapVisibility(viewA: View, viewB: View) {
-        val temp = viewA.visibility
-        viewA.visibility = viewB.visibility
-        viewB.visibility = temp
     }
 
-    fun clickCancelEvent() {
-        val output = "취소 되었습니다"
-        Toast.makeText(getApplicationContext(), output, Toast.LENGTH_SHORT).show()
     }
 
-    fun RequiredInputVerify() {
-        var output = "저장이 완료 되었습니다"
 
-        val phoneNumber = findViewById<EditText>(R.id.phone_number)
-        if (phoneNumber.text.toString() == "")
-            output = "전화번호는 필수 값입니다"
 
-        val name = findViewById<EditText>(R.id.name)
-        if (name.text.toString() == "")
-            output = "이름은 필수 값입니다"
 
-        Toast.makeText(getApplicationContext(), output, Toast.LENGTH_SHORT).show()
     }
 }
