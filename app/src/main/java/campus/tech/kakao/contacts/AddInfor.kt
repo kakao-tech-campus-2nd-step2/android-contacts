26b8f5909736772dac041a0ea4cc1ac01326fc0c
SHEEN <normengdie@pusan.ac.kr>
2024-06-28 02:00:31 +0900

    [bug fix] : add '}'

--- app/src/main/java/campus/tech/kakao/contacts/AddInfor.kt
+++ app/src/main/java/campus/tech/kakao/contacts/AddInfor.kt
@@ -65,198 +65,227 @@ class AddInfor : AppCompatActivity() {
         }
 
         cancelButton.setOnClickListener {
-            if(saveDataForCencel(inputName, inputPhoneNumber, inputEmail)){
-                alertDialog(this,toMain)
+            if (saveDataForCencel(inputName, inputPhoneNumber, inputEmail)) {
+                alertDialog(this, toMain)
 
-            }else{
+            } else {
                 cancelMessasge(cancelButton)
                 startActivity(toMain)
                 finish()
             }
         }
     }
-private fun saveMessage(saveButton: Button) {
-    Toast.makeText(saveButton.context, "저장이 완료 되었습니다.", Toast.LENGTH_SHORT).show()
-}
-private fun emptyDatamassage(saveButton: Button) {
-    Toast.makeText(saveButton.context, "이름과 전화번호는 필수 값입니다.", Toast.LENGTH_SHORT).show()
-}
-private fun cancelMessasge(cancelButton: Button) {
-    Toast.makeText(cancelButton.context, "취소 되었습니다.", Toast.LENGTH_SHORT).show()
-}
-private fun createSelectBirth(context: Context, hint: String): TextView {
-    return TextView(context).apply {
-        layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        ).apply {
-            setMargins(45, 16, 45, 16)
+
+    private fun saveMessage(saveButton: Button) {
+        Toast.makeText(saveButton.context, "저장이 완료 되었습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun emptyDatamassage(saveButton: Button) {
+        Toast.makeText(saveButton.context, "이름과 전화번호는 필수 값입니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun cancelMessasge(cancelButton: Button) {
+        Toast.makeText(cancelButton.context, "취소 되었습니다.", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun createSelectBirth(context: Context, hint: String): TextView {
+        return TextView(context).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            ).apply {
+                setMargins(45, 16, 45, 16)
+            }
+            text = hint
+            textSize = 18f
+            setBackgroundResource(R.drawable.edittextborder)
+            setOnClickListener {
+                selectDate(context, this)
+            }
         }
-        text = hint
-        textSize = 18f
-        setBackgroundResource(R.drawable.edittextborder)
-        setOnClickListener {
-            selectDate(context, this)
+    }
+
+    private fun selectDate(context: Context, textView: TextView) {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            context,
+            { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "${selectedYear}.${selectedMonth + 1}.${selectedDay}"
+                textView.text = selectedDate
+            },
+            year, month, day
+        )
+
+        datePickerDialog.show()
+    }
+
+    private fun createMemo(context: Context, hint: String): EditText {
+        val editText = EditText(context).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            ).apply {
+                setMargins(45, 16, 45, 16)
+            }
+            this.hint = hint
+            setBackgroundResource(R.drawable.edittextborder)
+        }
+        return editText
+    }
+
+    private fun createSelectGender(context: Context, hint: String): LinearLayout {
+        val container = LinearLayout(context).apply {
+            orientation = LinearLayout.HORIZONTAL
+            layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            ).apply {
+                setMargins(45, 16, 45, 16)
+            }
+            setBackgroundResource(R.drawable.edittextborder)
+            setPadding(16, 16, 16, 16)
+        }
+        val textView = TextView(context).apply {
+            layoutParams = LinearLayout.LayoutParams(
+                0,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                1f
+            )
+            this.text = hint
+            this.textSize = 18f
+        }
+        val radioGroup = RadioGroup(context).apply {
+            id = R.id.radioGroup
+            orientation = RadioGroup.HORIZONTAL
+            layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        }
+
+        val radioButtonMale = RadioButton(context).apply {
+            layoutParams = RadioGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            this.text = "Male"
+        }
+
+        val radioButtonFemale = RadioButton(context).apply {
+            layoutParams = RadioGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            this.text = "Female"
+        }
+
+        radioGroup.addView(radioButtonMale)
+        radioGroup.addView(radioButtonFemale)
+
+        container.addView(textView)
+        container.addView(radioGroup)
+
+        return container
+    }
+
+    private fun moreInformationBirth(context: Context, addInfor: LinearLayout) {
+        val textView = createSelectBirth(context, "생일")
+        birthTextView = textView
+        addInfor.addView(textView)
+    }
+
+    private fun moreInformationGender(context: Context, addInfor: LinearLayout) {
+        val layout = createSelectGender(context, "성별")
+        val radioGroup = layout.findViewById<RadioGroup>(R.id.radioGroup)
+        genderRadioGroup = radioGroup
+        addInfor.addView(layout)
+    }
+
+    private fun moreInformationMemo(context: Context, addInfor: LinearLayout) {
+        val editText = createMemo(context, "메모")
+        memoEditText = editText
+        addInfor.addView(editText)
+    }
+
+    private fun removeMoreInformationButton(button: View) {
+        val parentLayoutButton = button.parent as? ConstraintLayout
+        parentLayoutButton?.removeView(button)
+    }
+
+    private fun checkEmptyDataforSave(addName: String, addPhoneNumber: String): Boolean {
+        return addName.isNotEmpty() && addPhoneNumber.isNotEmpty()
+    }
+
+    private fun checkEmptyDataforCencel(
+        addName: String,
+        addPhoneNumber: String,
+        addEmail: String
+    ): Boolean {
+        return addName.isNotEmpty() || addPhoneNumber.isNotEmpty() || addEmail.isNotEmpty()
+    }
+
+    fun saveDataForSave(
+        inputName: EditText,
+        inputPhoneNumber: EditText,
+        inputEmail: EditText
+    ): Boolean {
+        addName = inputName.text.toString()
+        addPhoneNumber = inputPhoneNumber.text.toString()
+        addEmail = inputEmail.text.toString()
+        if (genderRadioGroup != null && birthTextView != null && memoEditText != null) {
+            addGender =
+                genderRadioGroup!!.findViewById<RadioButton>(genderRadioGroup!!.checkedRadioButtonId)?.text.toString()
+            addBirth = birthTextView!!.text.toString()
+            addMemo = memoEditText!!.text.toString()
+        }
+
+        return checkEmptyDataforSave(addName, addPhoneNumber)
+    }
+
+    fun saveDataForCencel(
+        inputName: EditText,
+        inputPhoneNumber: EditText,
+        inputEmail: EditText
+    ): Boolean {
+        addName = inputName.text.toString()
+        addPhoneNumber = inputPhoneNumber.text.toString()
+        addEmail = inputEmail.text.toString()
+        if (genderRadioGroup != null || birthTextView != null || memoEditText != null) {
+            addGender =
+                genderRadioGroup!!.findViewById<RadioButton>(genderRadioGroup!!.checkedRadioButtonId)?.text.toString()
+            addBirth = birthTextView!!.text.toString()
+            addMemo = memoEditText!!.text.toString()
+        }
+
+        return checkEmptyDataforCencel(addName, addPhoneNumber, addEmail)
+    }
+
+    private fun saveOrNotSave(checking: Boolean, saveButton: Button) {
+        if (checking) {
+            saveMessage(saveButton)
+        } else {
+            emptyDatamassage(saveButton)
         }
     }
-}
-private fun selectDate(context: Context, textView: TextView) {
-    val calendar = Calendar.getInstance()
-    val year = calendar.get(Calendar.YEAR)
-    val month = calendar.get(Calendar.MONTH)
-    val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-    val datePickerDialog = DatePickerDialog(
-        context,
-        { _, selectedYear, selectedMonth, selectedDay ->
-            val selectedDate = "${selectedYear}.${selectedMonth + 1}.${selectedDay}"
-            textView.text = selectedDate
-        },
-        year, month, day
-    )
+    fun alertDialog(context: Context, intent: Intent) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("타이틀 입니다.")
+            .setMessage("작성하시던 내용이 있습니다.")
+            .setPositiveButton("취소하기",
+                DialogInterface.OnClickListener { dialog, id ->
+                    context.startActivity(intent)
 
-    datePickerDialog.show()
-}
-private fun createMemo(context: Context, hint: String): EditText {
-    val editText = EditText(context).apply {
-        layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        ).apply {
-            setMargins(45, 16, 45, 16)
-        }
-        this.hint = hint
-        setBackgroundResource(R.drawable.edittextborder)
+                })
+            .setNegativeButton("돌아가기",
+                DialogInterface.OnClickListener { dialog, id ->
+                })
+        builder.show()
     }
-    return editText
-}
-private fun createSelectGender(context: Context, hint: String): LinearLayout {
-    val container = LinearLayout(context).apply {
-        orientation = LinearLayout.HORIZONTAL
-        layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        ).apply {
-            setMargins(45, 16, 45, 16)
-        }
-        setBackgroundResource(R.drawable.edittextborder)
-        setPadding(16, 16, 16, 16)
-    }
-    val textView = TextView(context).apply {
-        layoutParams = LinearLayout.LayoutParams(
-            0,
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            1f
-        )
-        this.text = hint
-        this.textSize = 18f
-    }
-    val radioGroup = RadioGroup(context).apply {
-        id = R.id.radioGroup
-        orientation = RadioGroup.HORIZONTAL
-        layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-    }
-
-    val radioButtonMale = RadioButton(context).apply {
-        layoutParams = RadioGroup.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        this.text = "Male"
-    }
-
-    val radioButtonFemale = RadioButton(context).apply {
-        layoutParams = RadioGroup.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        this.text = "Female"
-    }
-
-    radioGroup.addView(radioButtonMale)
-    radioGroup.addView(radioButtonFemale)
-
-    container.addView(textView)
-    container.addView(radioGroup)
-
-    return container
-}
-
-private fun moreInformationBirth(context: Context, addInfor: LinearLayout) {
-    val textView = createSelectBirth(context, "생일")
-    birthTextView = textView
-    addInfor.addView(textView)
-}
-
-private fun moreInformationGender(context: Context, addInfor: LinearLayout) {
-    val layout = createSelectGender(context, "성별")
-    val radioGroup = layout.findViewById<RadioGroup>(R.id.radioGroup)
-    genderRadioGroup = radioGroup
-    addInfor.addView(layout)
-}
-
-private fun moreInformationMemo(context: Context, addInfor: LinearLayout) {
-    val editText = createMemo(context, "메모")
-    memoEditText = editText
-    addInfor.addView(editText)
-}
-private fun removeMoreInformationButton(button: View) {
-    val parentLayoutButton = button.parent as? ConstraintLayout
-    parentLayoutButton?.removeView(button)
-}
-private fun checkEmptyDataforSave(addName: String, addPhoneNumber: String): Boolean {
-    return addName.isNotEmpty() && addPhoneNumber.isNotEmpty()
-}
-private fun checkEmptyDataforCencel(addName: String, addPhoneNumber: String,addEmail : String): Boolean {
-    return addName.isNotEmpty() || addPhoneNumber.isNotEmpty() || addEmail.isNotEmpty()
-}
-fun saveDataForSave(inputName: EditText, inputPhoneNumber: EditText, inputEmail: EditText): Boolean {
-    addName = inputName.text.toString()
-    addPhoneNumber = inputPhoneNumber.text.toString()
-    addEmail = inputEmail.text.toString()
-    if (genderRadioGroup != null && birthTextView != null && memoEditText != null) {
-        addGender = genderRadioGroup!!.findViewById<RadioButton>(genderRadioGroup!!.checkedRadioButtonId)?.text.toString()
-        addBirth = birthTextView!!.text.toString()
-        addMemo = memoEditText!!.text.toString()
-    }
-
-    return checkEmptyDataforSave(addName, addPhoneNumber)
-}
-fun saveDataForCencel(inputName: EditText, inputPhoneNumber: EditText, inputEmail: EditText): Boolean {
-    addName = inputName.text.toString()
-    addPhoneNumber = inputPhoneNumber.text.toString()
-    addEmail = inputEmail.text.toString()
-    if (genderRadioGroup != null || birthTextView != null || memoEditText != null) {
-        addGender = genderRadioGroup!!.findViewById<RadioButton>(genderRadioGroup!!.checkedRadioButtonId)?.text.toString()
-        addBirth = birthTextView!!.text.toString()
-        addMemo = memoEditText!!.text.toString()
-    }
-
-    return checkEmptyDataforCencel(addName, addPhoneNumber,addEmail)
-}
-private fun saveOrNotSave(checking: Boolean, saveButton: Button) {
-    if (checking) {
-        saveMessage(saveButton)
-    } else {
-        emptyDatamassage(saveButton)
-    }
-}
-fun alertDialog(context: Context,intent : Intent) {
-    val builder = AlertDialog.Builder(context)
-    builder.setTitle("타이틀 입니다.")
-        .setMessage("작성하시던 내용이 있습니다.")
-        .setPositiveButton("취소하기",
-            DialogInterface.OnClickListener { dialog, id ->
-                context.startActivity(intent)
-
-            })
-        .setNegativeButton("돌아가기",
-            DialogInterface.OnClickListener { dialog, id ->
-            })
-    builder.show()
 }
 private fun clearGlobalVariables() {
     addName = ""
@@ -268,4 +297,4 @@ private fun clearGlobalVariables() {
     memoEditText = null
     genderRadioGroup = null
     birthTextView = null
-}}
+}
