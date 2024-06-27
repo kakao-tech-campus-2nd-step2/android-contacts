03922a66200f72874b91a0a2e97b00ec6b697b7b
KSC <ksc1008@naver.com>
2024-06-27 14:12:10 +0900

    feat: add Contact data class

---
+++ app/src/main/java/campus/tech/kakao/contacts/model/Contact.kt
@@ -0,0 +1,5 @@
+package campus.tech.kakao.contacts.model
+
+import java.time.LocalDate
+
+data class Contact(var name:String, var phoneNumber:String, var email:String?, var gender:Int?, var birthday: LocalDate?, var memo:String?)
