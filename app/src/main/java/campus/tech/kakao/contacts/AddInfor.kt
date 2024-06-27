1d94b4e04cb7beb285c06608d9fd22e02de2227c
SHEEN <normengdie@pusan.ac.kr>
2024-06-27 21:42:42 +0900

    feat : add findViewId variable about wigets

--- app/src/main/java/campus/tech/kakao/contacts/AddInfor.kt
+++ app/src/main/java/campus/tech/kakao/contacts/AddInfor.kt
@@ -1,6 +1,9 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class AddInfor : AppCompatActivity() {
@@ -8,5 +11,12 @@ class AddInfor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_infor)
+        val forMoreInformation = findViewById<Button>(R.id.forMoreInformation)
+        val addInfor = findViewById<LinearLayout>(R.id.addInfor)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val inputEmail = findViewById<EditText>(R.id.inputEmail)
+        val cancelButton = findViewById<Button>(R.id.cancelButton)
+        val inputName = findViewById<EditText>(R.id.inputName)
+        val inputPhoneNumber = findViewById<EditText>(R.id.inputPhoneNumber)
     }
 }
