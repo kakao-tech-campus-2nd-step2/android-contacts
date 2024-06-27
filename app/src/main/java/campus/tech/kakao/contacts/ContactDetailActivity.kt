7c0d5714093b38d055b7fe5e76c7a2599e9fe612
유지예 <jiyeyu@192.168.nate.com>
2024-06-27 17:47:07 +0900

    feat: sub01[02] func7 - detail view

--- app/src/main/java/campus/tech/kakao/contacts/ContactDetailActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactDetailActivity.kt
@@ -1,4 +1,35 @@
 package campus.tech.kakao.contacts
 
-class ContactDetailActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.TextView
+
+class ContactDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.contact_detail)
+
+        //view 초기화 보는 거
+        val profileView = findViewById<ImageView>(R.id.profileView)
+        val nameView = findViewById<TextView>(R.id.nameView)
+        val phoneView = findViewById<TextView>(R.id.phoneView)
+        val emailView = findViewById<TextView>(R.id.emailView)
+        val birthView = findViewById<TextView>(R.id.birthView)
+        val genderView = findViewById<TextView>(R.id.genderView)
+        val memoView = findViewById<TextView>(R.id.memoView)
+
+        val nameLabelText = findViewById<TextView>(R.id.nameLabelText)
+        val phoneLabelText = findViewById<TextView>(R.id.phoneLabelText)
+        val emailLabelText = findViewById<TextView>(R.id.emailLabelText)
+        val birthLabelText = findViewById<TextView>(R.id.birthLabelText)
+        val genderLabelText = findViewById<TextView>(R.id.genderLabelText)
+        val memoLabelText = findViewById<TextView>(R.id.memoLabelText)
+
+        //parcel에 적어서 intent로 전달된 contact 객체 가져오기
+        val contact = intent.getParcelableExtra<Contact>("contact")
+
+        //연락처 정보 설정하기 - 조건
+
+    }
 }
