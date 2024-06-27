c704ac3cc233627a6816aefedf9587441753dd74
Kjamm <happyminig@gmail.com>
2024-06-27 14:51:38 +0900

    feat : 화면 하단에 FloatingAction 버튼 배치해 연락처 등록 화면으로 넘어가는 기능 추가

---
+++ app/src/main/java/campus/tech/kakao/contacts/AddContact.kt
@@ -0,0 +1,56 @@
+package campus.tech.kakao.contacts
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import org.w3c.dom.Text
+
+class AddContact : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var emptyView: TextView
+    private lateinit var fab : FloatingActionButton
+    private lateinit var contactsAdapter: ContactsAdapter
+    private val contactsList = mutableListOf<Contact>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contact)
+
+        recyclerView = findViewById(R.id.recycler)
+        emptyView = findViewById(R.id.empty)
+        fab = findViewById(R.id.fab)
+
+
+        fab.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_ADD_CONTACT)
+        }
+
+        updateUI()
+    }
+
+
+
+    private fun updateUI() {
+        if (contactsList.isEmpty()) {
+            recyclerView.visibility = View.GONE
+            emptyView.visibility = View.VISIBLE
+        } else {
+            recyclerView.visibility = View.VISIBLE
+            emptyView.visibility = View.GONE
+        }
+    }
+
+    companion object {
+        const val REQUEST_CODE_ADD_CONTACT = 1
+    }
+}
