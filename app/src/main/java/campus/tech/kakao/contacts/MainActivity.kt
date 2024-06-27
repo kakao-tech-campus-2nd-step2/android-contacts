ce3b5d784fdfc0e1957ae0707e8619fbdc9c3f30
jooiss <jusong513@pusan.ac.kr>
2024-06-27 18:30:52 +0900

    feat: Add name requirement

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,7 +1,11 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val nameInputForm = findViewById<EditText>(R.id.nameInputForm)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val cancelButton = findViewById<Button>(R.id.cancelButton)
+
+        saveButton.setOnClickListener {
+            if (nameInputForm.text.toString().trim().isEmpty()) {
+                Toast.makeText(this, "이름은 필수 값 입니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        cancelButton.setOnClickListener {
+            if (nameInputForm.text.toString().trim().isEmpty()) {
+                Toast.makeText(this, "이름은 필수 값 입니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
