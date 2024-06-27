90764987dd4f2dfe951d2626549cc3d4723acad5
todlf <todlfdl@naver.com>
2024-06-27 14:51:30 +0900

    feat: Add showing Toast Message
    - If you press cancle, save Button, show toast message
    - If nameEdit, numberEdit are empty, show toast message
    - Add adjustResize to cancle, saveButton on the keyboard

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val seeMoreButton = findViewById<Button>(R.id.seeMoreButton)
         seeMore(seeMoreButton)
+        showToast()
     }
-    private fun seeMore(seeMoreButton:Button){
+
+    private fun seeMore(seeMoreButton: Button) {
         seeMoreButton.setOnClickListener {
             val birthdayEdit = findViewById<EditText>(R.id.birthdayEdit)
             val genderEdit = findViewById<EditText>(R.id.genderEdit)
@@ -25,6 +28,25 @@ class MainActivity : AppCompatActivity() {
             memoEdit.visibility = EditText.VISIBLE
             genderRadio.visibility = RadioGroup.VISIBLE
         }
+    }
 
+    private fun showToast() {
+        val cancleButton = findViewById<Button>(R.id.cancleButton)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val nameEdit = findViewById<EditText>(R.id.nameEdit)
+        val numberEdit = findViewById<EditText>(R.id.numberEdit)
+        cancleButton.setOnClickListener {
+            Toast.makeText(this, "취소 되었습니다", Toast.LENGTH_SHORT).show()
+        }
+
+        saveButton.setOnClickListener {
+            if (nameEdit.text.isEmpty()) {
+                Toast.makeText(this, "이름은 필수 값 입니다.", Toast.LENGTH_SHORT).show()
+            } else if (numberEdit.text.isEmpty()) {
+                Toast.makeText(this, "전화번호는 필수 값 입니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "저장이 완료 되었습니다", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
