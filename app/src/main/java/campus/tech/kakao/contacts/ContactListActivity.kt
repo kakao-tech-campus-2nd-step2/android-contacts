16d3b759e2e8dec37c3f3a4671968db20273c85d
aengzu <jeon7879@naver.com>
2024-06-28 00:02:50 +0900

    feat: add ContactListActivity skeleton
    - Added file : ContactListActivity, activity_contact_list.xml

---
+++ app/src/main/java/campus/tech/kakao/contacts/ContactListActivity.kt
@@ -0,0 +1,29 @@
+package campus.tech.kakao.contacts
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_list)
+
+        // TODO : findViewById
+
+        // TODO : AddButton setOnClickListener
+
+        // TODO : RecyclerView Adapter 설정
+
+        }
+    }
+
+// TODO : RecyclerView Adapter 제작하기
+class ContactAdapter {
+
+// TODO : Item setOnClickListener
+
+}
