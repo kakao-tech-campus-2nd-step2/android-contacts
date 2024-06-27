62e75c4ac900b59a81e7c7403d177fd11d50a762
SHEEN <normengdie@pusan.ac.kr>
2024-06-27 21:50:19 +0900

    feat : created createSelectBirth, selectDate fun

--- app/src/main/java/campus/tech/kakao/contacts/AddInfor.kt
+++ app/src/main/java/campus/tech/kakao/contacts/AddInfor.kt
@@ -1,12 +1,23 @@
 package campus.tech.kakao.contacts
 
+import android.app.DatePickerDialog
+import android.content.Context
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+private var addName: String = ""
+private var addPhoneNumber: String = ""
+private var addEmail: String = ""
+private var addBirth: String = ""
+private var addMemo: String = ""
+private var addGender: String = ""
 class AddInfor : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,4 +40,37 @@ private fun emptyDatamassage(saveButton: Button) {
 }
 private fun cancelMessasge(cancelButton: Button) {
     Toast.makeText(cancelButton.context, "취소 되었습니다.", Toast.LENGTH_SHORT).show()
+}
+private fun createSelectBirth(context: Context, hint: String): TextView {
+    return TextView(context).apply {
+        layoutParams = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        ).apply {
+            setMargins(45, 16, 45, 16)
+        }
+        text = hint
+        textSize = 18f
+        setBackgroundResource(R.drawable.edittextborder)
+        setOnClickListener {
+            selectDate(context, this)
+        }
+    }
+}
+private fun selectDate(context: Context, textView: TextView) {
+    val calendar = Calendar.getInstance()
+    val year = calendar.get(Calendar.YEAR)
+    val month = calendar.get(Calendar.MONTH)
+    val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _, selectedYear, selectedMonth, selectedDay ->
+            val selectedDate = "${selectedYear}.${selectedMonth + 1}.${selectedDay}"
+            textView.text = selectedDate
+        },
+        year, month, day
+    )
+
+    datePickerDialog.show()
 }
