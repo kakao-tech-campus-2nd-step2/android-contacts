5520e2286d5859a5452b9e23f4505dcfd6426037
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-06-28 00:33:24 +0900

    refactor(AddContactActivity): 등록 화면 구현- 기존 MainActivity에 있던 요소를 AddContactActivity로 옮김

--- app/src/main/java/campus/tech/kakao/contacts/AddContactActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/AddContactActivity.kt
@@ -1,11 +1,53 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
 
 class AddContactActivity : AppCompatActivity() {
+
+    val contactManager = ContactManager()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_contact)
+
+            val name = findViewById<EditText>(R.id.name)
+            val phoneNumber = findViewById<EditText>(R.id.phoneNumber)
+            val email = findViewById<EditText>(R.id.email)
+            val birthDay = findViewById<EditText>(R.id.birthDay)
+            val gender = findViewById<RadioGroup>(R.id.gender)
+            val memo = findViewById<EditText>(R.id.memo)
+
+            val birthDayLayout = findViewById<LinearLayoutCompat>(R.id.birthDayLayout)
+            val genderLayout = findViewById<LinearLayoutCompat>(R.id.genderLayout)
+            val memoLayout = findViewById<LinearLayoutCompat>(R.id.memoLayout)
+
+            val more = findViewById<LinearLayoutCompat>(R.id.more)
+            val save = findViewById<TextView>(R.id.save)
+            val cancle = findViewById<TextView>(R.id.cancle)
+
+            more.setOnClickListener {
+                birthDayLayout.visibility = View.VISIBLE
+                genderLayout.visibility = View.VISIBLE
+                memoLayout.visibility = View.VISIBLE
+                more.visibility = View.GONE
+            }
+
+            cancle.setOnClickListener {
+                contactManager.showToast(this@AddContactActivity, "취소되었습니다.")
+            }
+
+            save.setOnClickListener {
+                if(contactManager.checkIsFilled(name.getText().toString())&&contactManager.checkIsFilled(phoneNumber.getText().toString()))
+                {
+                    contactManager.showToast(this@AddContactActivity, "저장되었습니다.")
+                } else {
+                    contactManager.showToast(this@AddContactActivity, "이름과 번호 입력은 필수입니다.")
+                }
+            }
     }
 }
--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -16,42 +16,5 @@
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
 
-
-
-            val name = findViewById<EditText>(R.id.name)
-            val phoneNumber = findViewById<EditText>(R.id.phoneNumber)
-            val email = findViewById<EditText>(R.id.email)
-            val birthDay = findViewById<EditText>(R.id.birthDay)
-            val gender = findViewById<RadioGroup>(R.id.gender)
-            val memo = findViewById<EditText>(R.id.memo)
-
-            val birthDayLayout = findViewById<LinearLayoutCompat>(R.id.birthDayLayout)
-            val genderLayout = findViewById<LinearLayoutCompat>(R.id.genderLayout)
-            val memoLayout = findViewById<LinearLayoutCompat>(R.id.memoLayout)
-
-            val more = findViewById<LinearLayoutCompat>(R.id.more)
-            val save = findViewById<TextView>(R.id.save)
-            val cancle = findViewById<TextView>(R.id.cancle)
-
-            more.setOnClickListener {
-                birthDayLayout.visibility = View.VISIBLE
-                genderLayout.visibility = View.VISIBLE
-                memoLayout.visibility = View.VISIBLE
-                more.visibility = View.GONE
-            }
-
-            cancle.setOnClickListener {
-                contactManager.showToast(this@MainActivity, "취소되었습니다.")
-            }
-
-            save.setOnClickListener {
-                if(contactManager.checkIsFilled(name.getText().toString())&&contactManager.checkIsFilled(phoneNumber.getText().toString()))
-                {
-                    Log.d("textField", name.getText().toString())
-                    contactManager.showToast(this@MainActivity, "저장되었습니다.")
-                } else {
-                    contactManager.showToast(this@MainActivity, "이름과 번호 입력은 필수입니다.")
-                }
-            }
         }
     }
