7f4d3d15e28d13e767908af074cce54b576fc34c
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2024-06-27 16:31:04 +0900

    Implemented basic functionality in MainActivity, excluding additional field interactions.
    - Designed the main layout with input fields for name, phone number, and email.
    - Integrated 'More' button for expanding additional options, pending further functionality.
    
    This commit lays the foundation for handling core features of capturing contact information and basic interaction flows in MainActivity.

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,47 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nameField: EditText = findViewById(R.id.name_field)
+        val phoneField: EditText = findViewById(R.id.phone_field)
+        val emailField: EditText = findViewById(R.id.email_field)
+        val saveButton: Button = findViewById(R.id.save_button)
+        val cancelButton: Button = findViewById(R.id.cancel_button)
+        val moreButton: LinearLayout = findViewById(R.id.more_button)
+        val additionalFields: LinearLayout = findViewById(R.id.additional_fields)
+
+
+        moreButton.setOnClickListener {
+            moreButton.visibility = View.GONE
+            additionalFields.visibility = View.VISIBLE
+        }
+
+        saveButton.setOnClickListener {
+            val name = nameField.text.toString()
+            val phone = phoneField.text.toString()
+            val email = emailField.text.toString()
+
+            if (name.isEmpty()) {
+                Toast.makeText(this, getString(R.string.toast_name_is_essential), Toast.LENGTH_SHORT).show()
+            } else if (phone.isEmpty()) {
+                Toast.makeText(this,
+                    getString(R.string.toast_phone_number_is_essential), Toast.LENGTH_SHORT).show()
+            } else {
+                var message = getString(R.string.toast_saved)
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            }
+        }
+        cancelButton.setOnClickListener {
+            Toast.makeText(this, getString(R.string.toast_canceled), Toast.LENGTH_SHORT).show()
+        }
     }
 }
