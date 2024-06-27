29e4d399348d557a37cec6f719e947026b79b875
khyeonm <122lamb@naver.com>
2024-06-27 20:25:47 +0900

    feat: Add interaction between Detail and MainActivity

--- app/src/main/java/campus/tech/kakao/contacts/ContactDetail.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactDetail.kt
@@ -1,6 +1,7 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +11,20 @@ class ContactDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_detail)
+
+        val name = intent.getStringExtra("name")
+        val phone = intent.getStringExtra("phone")
+        val mail = intent.getStringExtra("email")
+        val birth = intent.getStringExtra("birthday")
+        val gender = intent.getStringExtra("gender")
+        val memo = intent.getStringExtra("memo")
+
+        findViewById<TextView>(R.id.detailname).text = name
+        findViewById<TextView>(R.id.detailphone).text = phone
+        findViewById<TextView>(R.id.detailmail).text = mail
+        findViewById<TextView>(R.id.detailbirth).text = birth
+        findViewById<TextView>(R.id.detailgender).text = gender
+        findViewById<TextView>(R.id.detailmemo).text = memo
 
     }
 }
