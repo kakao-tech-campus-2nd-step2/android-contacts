872a53db5da6fe6bf1240f6ad912927e9d7af59e
KSC <ksc1008@naver.com>
2024-06-27 17:09:12 +0900

    feat: shows message when there is no contacts in the list

--- app/src/main/java/campus/tech/kakao/contacts/ContactsListActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactsListActivity.kt
@@ -6,7 +6,9 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -20,14 +22,17 @@ import java.time.LocalDate
 class ContactsListActivity : AppCompatActivity() {
     val contactList: MutableList<Contact> = mutableListOf()
     lateinit var recyclerView: RecyclerView
-    val activityLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-        if(it.resultCode == RESULT_OK) {
-            val contact = getContactFromIntent(it.data)
-            if (contact != null) {
-                addNewContact(contact)
+    lateinit var helpMessageText: TextView
+
+    val activityLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val contact = getContactFromIntent(it.data)
+                if (contact != null) {
+                    addNewContact(contact)
+                }
             }
         }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,23 +40,34 @@ class ContactsListActivity : AppCompatActivity() {
 
         initiateRecyclerView()
         initiateAddContactButton()
-        testAddContacts()
+        //testAddContacts()
+        helpMessageText = findViewById(R.id.text_help)
     }
 
-    fun testAddContacts(){
-        addNewContact(Contact("권성찬", "010123123", "kscksc@ksc.com", AddContactActivity.GENDER_MALE, LocalDate.now(), "Hello!!"))
+    fun testAddContacts() {
+        addNewContact(
+            Contact(
+                "권성찬",
+                "010123123",
+                "kscksc@ksc.com",
+                AddContactActivity.GENDER_MALE,
+                LocalDate.now(),
+                "Hello!!"
+            )
+        )
         addNewContact(Contact("박병호", "4442358", null, null, null, null))
     }
 
     fun initiateRecyclerView() {
         recyclerView = findViewById(R.id.contact_list_recyclerView)
-        recyclerView.adapter = ContactListAdapter(LayoutInflater.from(this), this, contactList){ _, index ->
-            clickContactItem(contactList[index])
-        }
+        recyclerView.adapter =
+            ContactListAdapter(LayoutInflater.from(this), this, contactList) { _, index ->
+                clickContactItem(contactList[index])
+            }
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 
-    fun initiateAddContactButton(){
+    fun initiateAddContactButton() {
         val addContactButton = findViewById<Button>(R.id.button_add_contact)
         addContactButton.setOnClickListener {
             startAddContactActivity()
@@ -61,13 +77,19 @@ class ContactsListActivity : AppCompatActivity() {
     fun addNewContact(contact: Contact) {
         contactList.add(contact)
         recyclerView.adapter?.notifyItemInserted(contactList.size)
+
+        setHelpMessageActive(false)
     }
 
     fun removeContact(contact: Contact) {
         contactList.remove(contact)
+
+        if (contactList.size == 0) {
+            setHelpMessageActive(true)
+        }
     }
 
-    fun startAddContactActivity(){
+    fun startAddContactActivity() {
         val intent = Intent()
         val componentName: ComponentName = ComponentName(
             this@ContactsListActivity,
@@ -77,7 +99,7 @@ class ContactsListActivity : AppCompatActivity() {
         activityLauncher.launch(intent)
     }
 
-    fun startShowContactInfoActivity(contact: Contact){
+    fun startShowContactInfoActivity(contact: Contact) {
         val intent = Intent()
         val componentName = ComponentName(
             this@ContactsListActivity,
@@ -88,18 +110,28 @@ class ContactsListActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun getContactFromIntent(intent: Intent?): Contact?{
-        if(intent == null)
+    fun getContactFromIntent(intent: Intent?): Contact? {
+        if (intent == null)
             return null
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            intent.getSerializableExtra(AddContactActivity.KEY_CONTACT, Contact::class.java) as Contact
-        } else{
+            intent.getSerializableExtra(
+                AddContactActivity.KEY_CONTACT,
+                Contact::class.java
+            ) as Contact
+        } else {
             intent.getSerializableExtra(AddContactActivity.KEY_CONTACT) as Contact?
         }
     }
 
-    fun clickContactItem(contactItem:Contact){
-        Log.d("KSC", "Name: ${contactItem.name}, Phone: ${contactItem.phoneNumber}\nBirthday: ${contactItem.birthday?:"NULL"},Email: ${contactItem.email?:"NULL"}")
+    fun clickContactItem(contactItem: Contact) {
+        Log.d(
+            "KSC",
+            "Name: ${contactItem.name}, Phone: ${contactItem.phoneNumber}\nBirthday: ${contactItem.birthday ?: "NULL"},Email: ${contactItem.email ?: "NULL"}"
+        )
         startShowContactInfoActivity(contactItem)
+    }
+
+    fun setHelpMessageActive(active: Boolean) {
+        helpMessageText.visibility = if (active) View.VISIBLE else View.GONE
     }
 }
