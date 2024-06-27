b7a0846e439dabf98b45299fde1e8769b2f21c4d
SHEEN <normengdie@pusan.ac.kr>
2024-06-28 01:40:44 +0900

    feat : add receivedInfor fun

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,12 +1,28 @@
 package campus.tech.kakao.contacts
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val contactList: MutableList<ContactInfo> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    private fun receivedInfor(intent: Intent?) {
+        intent?.let {
+            val addName = it.getStringExtra("inforname") ?: "Null"
+            val addPhoneNumber = it.getStringExtra("infornumber") ?: "null"
+            val addEmail = it.getStringExtra("inforEmail") ?: ""
+            val addBirth = it.getStringExtra("inforBirth") ?: ""
+            val addMemo = it.getStringExtra("inforMemo") ?: ""
+            val addGender = it.getStringExtra("inforGender") ?: ""
+
+            val contactInfo =
+                ContactInfo(addName, addPhoneNumber, addEmail, addBirth, addMemo, addGender)
+            contactList.add(contactInfo)
+        }
     }
 }
 data class ContactInfo(
