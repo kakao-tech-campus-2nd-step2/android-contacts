258738b955ede0bbf0e410535261769c5d168928
todlf <todlfdl@naver.com>
2024-06-27 14:18:29 +0900

    feat: Add seeMoreButton Press
    - Add hint font faded
    - Add seeMoreButton press function

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,30 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val seeMoreButton = findViewById<Button>(R.id.seeMoreButton)
+        seeMore(seeMoreButton)
+    }
+    private fun seeMore(seeMoreButton:Button){
+        seeMoreButton.setOnClickListener {
+            val birthdayEdit = findViewById<EditText>(R.id.birthdayEdit)
+            val genderEdit = findViewById<EditText>(R.id.genderEdit)
+            val memoEdit = findViewById<EditText>(R.id.memoEdit)
+            val genderRadio = findViewById<RadioGroup>(R.id.genderRadio)
+            birthdayEdit.visibility = EditText.VISIBLE
+            genderEdit.visibility = EditText.VISIBLE
+            memoEdit.visibility = EditText.VISIBLE
+            genderRadio.visibility = RadioGroup.VISIBLE
+        }
+
     }
 }
