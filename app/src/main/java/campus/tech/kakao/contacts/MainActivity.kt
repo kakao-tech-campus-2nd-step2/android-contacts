4aa7b8b93900b572f8d04d55d1c4d87c664692c1
heonniy <dkgus7535@naver.com>
2024-06-27 15:06:15 +0900

    3. feat : 이름과 전화번호를 입력하지 않을시 토스트 메시지 보내기 구현

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,45 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
+import org.w3c.dom.Text
+import java.util.jar.Attributes.Name
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var NameEdit : EditText = findViewById(R.id.NameEdit)
+        var PhoneNumberEdit : EditText = findViewById(R.id.PhoneNumberEdit)
+        var MoreText : TextView = findViewById(R.id.MoreText)
+        var BirthEdit : EditText = findViewById(R.id.BirthEdit)
+        var MemoEdit : EditText = findViewById(R.id.MemoEdit)
+        var GenderArea : LinearLayoutCompat = findViewById(R.id.GenderArea)
+        var SaveText : TextView = findViewById(R.id.SaveText)
+        var CancleText : TextView = findViewById(R.id.CancleText)
+
+        
+        SaveText.setOnClickListener {
+            if (NameEdit.text.toString().trim().isEmpty()){
+                Toast
+                    .makeText(this@MainActivity,"이름은 필수값입니다.",Toast.LENGTH_SHORT)
+                    .show()
+            }
+            if (PhoneNumberEdit.text.toString().trim().isEmpty()){
+                Toast
+                    .makeText(this@MainActivity,"전화번호는 필수값입니다.",Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            
+        }
+
     }
 }
