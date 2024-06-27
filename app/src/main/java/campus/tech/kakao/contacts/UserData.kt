5f3a797ebd323082d85cf2fa2abb111248f2c37e
Jeong hoon <hoondb@naver.com>
2024-06-28 08:27:51 +0900

    feat: Add new fields to UserData
    - 이메일, 생일, 성별, 메모 필드 추가

--- app/src/main/java/campus/tech/kakao/contacts/UserData.kt
+++ app/src/main/java/campus/tech/kakao/contacts/UserData.kt
@@ -3,5 +3,9 @@ package campus.tech.kakao.contacts
 import java.io.Serializable
 data class UserData(
     val name: String,
-    val phoneNumber: Int
+    val phoneNumber: Int,
+    val email: String,
+    val birthday: String,
+    val gender: String,
+    val memo: String
 ) : Serializable
