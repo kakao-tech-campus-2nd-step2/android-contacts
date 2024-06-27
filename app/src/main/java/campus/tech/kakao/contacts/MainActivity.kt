2db7888eea5ac13cd55f45bcc0d3c3cdc045db5b
todlf <todlfdl@naver.com>
2024-06-27 23:31:57 +0900

    feat: Add CalendarView
    - Change birthdayEdit to birthdayButton
    - Add showing calendar when you click birthdayButton

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,17 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,18 +21,22 @@ class MainActivity : AppCompatActivity() {
         val seeMoreButton = findViewById<Button>(R.id.seeMoreButton)
         seeMore(seeMoreButton)
         showToast()
+        clickDate()
     }
 
     private fun seeMore(seeMoreButton: Button) {
         seeMoreButton.setOnClickListener {
-            val birthdayEdit = findViewById<EditText>(R.id.birthdayEdit)
             val genderEdit = findViewById<EditText>(R.id.genderEdit)
             val memoEdit = findViewById<EditText>(R.id.memoEdit)
             val genderRadio = findViewById<RadioGroup>(R.id.genderRadio)
-            birthdayEdit.visibility = EditText.VISIBLE
+            val birthdayText = findViewById<TextInputLayout>(R.id.birthdayText)
+            val birthdayButton = findViewById<Button>(R.id.birthdayButton)
             genderEdit.visibility = EditText.VISIBLE
             memoEdit.visibility = EditText.VISIBLE
             genderRadio.visibility = RadioGroup.VISIBLE
+            birthdayText.visibility = TextInputLayout.VISIBLE
+            birthdayButton.visibility = Button.VISIBLE
+            seeMoreButton.visibility = Button.INVISIBLE
         }
     }
 
@@ -47,6 +57,23 @@ class MainActivity : AppCompatActivity() {
             } else {
                 Toast.makeText(this, "저장이 완료 되었습니다", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun clickDate() {
+        val birthdayCalendar = findViewById<CalendarView>(R.id.birthdayCalendar)
+        val birthdayButton = findViewById<Button>(R.id.birthdayButton)
+        val frm = findViewById<FrameLayout>(R.id.frm)
+        birthdayButton.setOnClickListener {
+            Log.d("testt", "birthdayEdit clicked")
+            birthdayCalendar.bringToFront()
+            birthdayCalendar.visibility = View.VISIBLE
+            frm.visibility = View.VISIBLE
+        }
+        birthdayCalendar.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val selectedDate = "$year-${month + 1}-$dayOfMonth"
+            birthdayButton.setText(selectedDate)
+            birthdayCalendar.visibility = View.GONE
         }
     }
 }
