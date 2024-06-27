2c7c2203e8e81e0fb1c96ff282367afafe9ff5b5
ddangcong80 <ddangcong80@gmail.com>
2024-06-27 15:15:10 +0900

    feat: 연락처 이름 입력 기능 구현 및 토스트 메세지 표시 기능 추가

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,43 @@
 package campus.tech.kakao.contacts
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.ButtonBarLayout
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name: EditText
+    lateinit var cancel: Button
+    lateinit var save: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViews()
+
+        cancel.setOnClickListener {
+            name.setText(null);
+            Toast.makeText(this, "취소 되었습니다", Toast.LENGTH_LONG).show()
+        }
+
+        save.setOnClickListener {
+            if (name.text.isEmpty()) {
+                Toast.makeText(this, "이름을 반드시 적어야 합니다", Toast.LENGTH_LONG).show()
+            }
+            else {
+                Toast.makeText(this, "저장이 완료 되었습니다", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+    fun findViews() {
+        name = findViewById(R.id.name)
+        cancel = findViewById(R.id.cancel)
+        save = findViewById(R.id.save)
     }
 }
