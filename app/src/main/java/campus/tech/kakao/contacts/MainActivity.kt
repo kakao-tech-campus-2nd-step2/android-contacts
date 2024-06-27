bc215b779ccc513d48c620d8d7ae6cc6d20accac
SHEEN <normengdie@pusan.ac.kr>
2024-06-28 01:42:01 +0900

    feat : add class ContactAdapter

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,22 @@
 package campus.tech.kakao.contacts
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private val contactList: MutableList<ContactInfo> = mutableListOf()
+    private lateinit var addInforLauncher: ActivityResultLauncher<Intent>
+    private lateinit var listView: ListView
+    private lateinit var adapter: ContactAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +34,22 @@ class MainActivity : AppCompatActivity() {
                 ContactInfo(addName, addPhoneNumber, addEmail, addBirth, addMemo, addGender)
             contactList.add(contactInfo)
         }
+    }
+}
+class ContactAdapter(context: Context, private val data: List<ContactInfo>) :
+    ArrayAdapter<ContactInfo>(context, 0, data) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val itemView = convertView ?: LayoutInflater.from(context).inflate(R.layout.contact_items, parent, false)
+
+        val contact = getItem(position)
+        val textViewName = itemView.findViewById<TextView>(R.id.textViewId)
+        val textViewName2 = itemView.findViewById<TextView>(R.id.textViewName)
+        val contactName = contact?.name
+        textViewName.text = contactName?.firstOrNull()?.toString() ?: ""
+        textViewName2.text = contact?.name
+
+        return itemView
     }
 }
 data class ContactInfo(
