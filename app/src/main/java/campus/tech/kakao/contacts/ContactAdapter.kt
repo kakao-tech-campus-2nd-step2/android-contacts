7b0836fdd7d3bf73ad1c3ca55968637f168b3c01
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-06-28 03:25:46 +0900

    feat(ContactAdapter): 리사이클러 뷰 어댑터 ContactAdapter 생성

---
+++ app/src/main/java/campus/tech/kakao/contacts/ContactAdapter.kt
@@ -0,0 +1,35 @@
+package campus.tech.kakao.contacts
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter(val itemList: List<Contact>,
+                     var inflater: LayoutInflater
+): RecyclerView.Adapter<ContactAdapter.ContactViewHolder>() {
+    inner class ContactViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val name: TextView
+        val phoneNumber: TextView
+        init {
+            name = itemView.findViewById<TextView>(R.id.name)
+            phoneNumber= itemView.findViewById<TextView>(R.id.phoneNumber)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = inflater.inflate(R.layout.contact_item, parent, false)
+        return ContactViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        holder.name.text = itemList[position].name
+        holder.phoneNumber.text = itemList[position].phoneNumber
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+}
