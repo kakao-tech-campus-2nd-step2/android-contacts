14ad72f7aa1324207397cb4a37e1725cc1a9fd24
leeghy <epuy109@gmail.com>
2024-06-27 14:47:31 +0900

    생일 누르면 캘린더 띄우기

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,8 +1,11 @@
 package campus.tech.kakao.contacts
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -56,6 +59,9 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show()
             }
         }
+        birth.setOnClickListener {
+            showCalendar()
+        }
     }
 
     private fun expandView() {
@@ -78,6 +84,18 @@ class MainActivity : AppCompatActivity() {
         else {
             return true
         }
+    }
+
+    private fun showCalendar() {
+        var dateString = ""
+        val cal = Calendar.getInstance()
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            dateString = "${year}.${month+1}.${dayOfMonth}"
+            birth.setText(dateString)
+
+        }
+        DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
     }
 
 
