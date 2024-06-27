f10743bb527c89a1fd9f3b49ec9f536556cd3c9a
todlf <todlfdl@naver.com>
2024-06-28 00:05:58 +0900

    feat: Add plusButton intent

--- app/src/main/java/campus/tech/kakao/contacts/contactsList.kt
+++ app/src/main/java/campus/tech/kakao/contacts/contactsList.kt
@@ -1,6 +1,8 @@
 package campus.tech.kakao.contacts
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,11 @@ class contactsList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts_list)
+        val addButton = findViewById<Button>(R.id.addButton)
+        addButton.setOnClickListener {
+            Intent(this, MainActivity::class.java).let{
+                startActivity(it)
+            }
+        }
     }
 }
