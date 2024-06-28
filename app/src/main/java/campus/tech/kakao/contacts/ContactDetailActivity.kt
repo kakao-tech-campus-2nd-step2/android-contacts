0c84e9b93f008257d7899406a607688a5f42ca55
akuby21 <39649594+akuby21@users.noreply.github.com>
2024-06-28 12:48:07 +0900

    feat: implement ContactDetailActivity
    저장한 연락처의 상세한 정보를 볼 수 있는 ContactDetailActivity 생성
    ContactListActivity에서 선택 시 repository내 contact의 인덱스를 보내어 액티비티에서 식별한다

---
+++ app/src/main/java/campus/tech/kakao/contacts/ContactDetailActivity.kt
@@ -0,0 +1,83 @@
+package campus.tech.kakao.contacts
+
+import android.os.Bundle
+import android.view.View.VISIBLE
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ContactDetailActivity : AppCompatActivity() {
+
+    fun handleMail(mail : String?){
+        val mailLayout = findViewById<RelativeLayout>(R.id.mailLayout)
+        if(!mail.isNullOrEmpty()){
+            mailLayout.visibility = VISIBLE
+            val mailView = findViewById<TextView>(R.id.mail)
+            mailView.text = mail
+
+        }
+    }
+
+    fun handleBirthday(birthday : String?){
+        val birthdayLayout = findViewById<RelativeLayout>(R.id.birthdayLayout)
+        if(!birthday.isNullOrEmpty()){
+            birthdayLayout.visibility = VISIBLE
+            val birthdayView = findViewById<TextView>(R.id.birthday)
+            birthdayView.text = birthday
+
+        }
+    }
+
+    fun handleGender(gender: Gender?){
+        val genderLayout = findViewById<RelativeLayout>(R.id.genderLayout)
+        if(gender != null){
+            genderLayout.visibility = VISIBLE
+            val genderView = findViewById<TextView>(R.id.gender)
+            genderView.text = gender.name
+
+        }
+    }
+
+    fun handleMemo(memo : String?){
+        val memoLayout = findViewById<RelativeLayout>(R.id.memoLayout)
+        if(!memo.isNullOrEmpty()){
+            memoLayout.visibility = VISIBLE
+            val memoView = findViewById<TextView>(R.id.memo)
+            memoView.text = memo
+
+        }
+    }
+
+
+    fun loadContact(contact: Contact){
+        val name = findViewById<TextView>(R.id.name)
+        val tel = findViewById<TextView>(R.id.tel)
+        name.text = contact.name
+        tel.text = contact.tel
+
+
+        handleMail(contact.mail)
+        handleBirthday(contact.bDay)
+        handleGender(contact.gender)
+        handleMemo(contact.memo)
+    }
+
+    fun showError(){
+        val error = findViewById<TextView>(R.id.error)
+        error.visibility = VISIBLE
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_detail)
+
+        when(val position = intent.extras?.getInt("position")){
+            null -> showError()
+            else -> {
+                val contact = contactRepository.contactList[position]
+                loadContact(contact)
+            }
+        }
+
+    }
+}
