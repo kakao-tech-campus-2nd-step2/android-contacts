85455b65058650ab259cc2910b4371f5d2e90f73
jooiss <jusong513@pusan.ac.kr>
2024-06-28 17:15:54 +0900

    feat: Add data transmission function of save button

--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,12 +1,14 @@
 package campus.tech.kakao.contacts
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -18,9 +20,12 @@ class MainActivity : AppCompatActivity() {
 
         val nameInputForm = findViewById<EditText>(R.id.nameInputForm)
         val phoneInputForm = findViewById<EditText>(R.id.phoneInputForm)
+        val emailInputForm = findViewById<EditText>(R.id.emailInputForm)
         val moreButton = findViewById<LinearLayout>(R.id.moreButton)
         val moreInformationForm = findViewById<LinearLayout>(R.id.moreInformationForm)
         val birthdayInputForm = findViewById<TextView>(R.id.birthdayInputForm)
+        val genderInputForm = findViewById<RadioGroup>(R.id.genderRadioGroup)
+        val memoInputForm = findViewById<EditText>(R.id.memoInputForm)
         val saveButton = findViewById<Button>(R.id.saveButton)
         val cancelButton = findViewById<Button>(R.id.cancelButton)
 
@@ -48,7 +53,23 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "전화 번호는 필수 값 입니다.", Toast.LENGTH_SHORT).show()
             }
             else {
+                val name = nameInputForm.text.toString()
+                val phone = phoneInputForm.text.toString()
+                val email = emailInputForm.text.toString()
+                val birthday = birthdayInputForm.text.toString()
+                val gender = genderInputForm.checkedRadioButtonId.toString()
+                val memo = memoInputForm.text.toString()
+                val intent = Intent().apply {
+                    putExtra("name", name)
+                    putExtra("phone", phone)
+                    putExtra("email", email)
+                    putExtra("birthday", birthday)
+                    putExtra("gender", gender)
+                    putExtra("memo", memo)
+                }
+                setResult(RESULT_OK, intent)
                 Toast.makeText(this, "저장이 완료 되었습니다.", Toast.LENGTH_SHORT).show()
+                finish()
             }
         }
 
