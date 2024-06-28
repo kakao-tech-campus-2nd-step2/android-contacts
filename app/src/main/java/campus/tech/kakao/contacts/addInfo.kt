10aebb7896fccc62ae75abd5a241040613dc47e6
leeghy <epuy109@gmail.com>
2024-06-28 14:36:01 +0900

    연락처 추가 뷰 구성/플러스 버튼 누르면 화면 넘어가는 것 구현

---
+++ app/src/main/java/campus/tech/kakao/contacts/addInfo.kt
@@ -0,0 +1,39 @@
+package campus.tech.kakao.contacts
+
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class addInfo : AppCompatActivity() {
+    private lateinit var addBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_info)
+
+        initialize()
+        setUpListeners()
+
+    }
+
+    private fun initialize() {
+        addBtn = findViewById(R.id.addBtn)
+    }
+
+    private fun setUpListeners() {
+        addBtn.setOnClickListener {
+            val intent: Intent = Intent()
+            val componentName: ComponentName = ComponentName(
+                "campus.tech.kakao.contacts",
+                "campus.tech.kakao.contacts.MainActivity"
+            )
+            intent.component = componentName
+            startActivity(intent)
+        }
+    }
+}
