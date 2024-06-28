4da8bd7b8f4e848de7be55368dadda34d40a39b8
aengzu <jeon7879@naver.com>
2024-06-28 14:25:47 +0900

    feat: add phone detail view

---
+++ app/src/main/java/campus/tech/kakao/contacts/ContactDetailActivity.kt
@@ -0,0 +1,15 @@
+package campus.tech.kakao.contacts
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ContactDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_detail)
+
+        }
+    }
