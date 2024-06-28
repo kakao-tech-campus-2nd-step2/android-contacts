bcb132f724fba8bbcff39ce6d3195da9006205de
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-28 16:37:37 +0900

    feat: Add displayContact() to show saved contacts on screen

--- app/src/main/java/campus/tech/kakao/contacts/ContactListActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactListActivity.kt
@@ -2,15 +2,23 @@ package campus.tech.kakao.contacts
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ContactListActivity : AppCompatActivity() {
-    
+
     lateinit var plusbtn: Button
+    lateinit var infotext : TextView
+    val personList = mutableListOf<Person>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +31,12 @@ class ContactListActivity : AppCompatActivity() {
         }
         setUI()
         goToAddContact()
+        displayContact()
     }
 
     private fun setUI(){
         plusbtn = findViewById(R.id.btn_plus)
+        infotext = findViewById(R.id.text_info)
     }
 
     fun goToAddContact() {
@@ -34,5 +44,52 @@ class ContactListActivity : AppCompatActivity() {
             val intent = Intent(this, SaveContactActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun displayContact(){
+        val name = intent.getStringExtra("name")
+        val phone = intent.getStringExtra("phone")
+
+        if (name != null && phone != null) {
+            infotext.visibility = View.GONE
+
+            personList.add(Person(name.toString(), phone))
+            with(findViewById<RecyclerView>(R.id.contactRecyclerView)) {
+                this.layoutManager = LinearLayoutManager(this@ContactListActivity)
+                this.adapter = PersonAdapter(
+                    personList = personList,
+                    inflater = LayoutInflater.from(this@ContactListActivity)
+                )
+            }
+        }
+    }
+}
+class PersonAdapter(
+    val personList: MutableList<Person>,
+    val inflater: LayoutInflater
+):RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    class ViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
+        val text_name: TextView
+        val text_initial: TextView
+        init{
+            text_name = itemView.findViewById(R.id.name)
+            text_initial = itemView.findViewById(R.id.nameinitial)
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = inflater.inflate(R.layout.activity_person_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val person = personList.get(position)
+        val viewHolder = holder as ViewHolder
+        viewHolder.text_name.text = person.name
+        viewHolder.text_initial.text = person.name[0].toString()
+    }
+
+    override fun getItemCount(): Int {
+        return personList.size
     }
 }
