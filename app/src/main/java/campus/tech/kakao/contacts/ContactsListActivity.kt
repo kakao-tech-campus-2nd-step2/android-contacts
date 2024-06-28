2eafb0f1716f08da0f235f2844e6ea7577a10f4f
Jeong Suhyeon <jsh00325@naver.com>
2024-06-28 20:45:51 +0900

    feat: Create Adapter for ContactsList & Connect with RecyclerView

--- app/src/main/java/campus/tech/kakao/contacts/ContactsListActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactsListActivity.kt
@@ -1,15 +1,34 @@
 package campus.tech.kakao.contacts
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ContactsListActivity : AppCompatActivity() {
+	lateinit var contactsList: RecyclerView
+	lateinit var emptyInfoView: TextView
+	lateinit var addButton: FloatingActionButton
+
+	val contactsDataList = mutableListOf<ContactData>()
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_contacts_list)
 
+		contactsList = findViewById(R.id.contacts_list_recyclerview)
+		emptyInfoView = findViewById(R.id.contact_list_empty_textview)
+		addButton = findViewById(R.id.contact_add_button)
+
+		contactsList.adapter = ContactsListAdapter(contactsDataList, LayoutInflater.from(this))
+		contactsList.layoutManager = LinearLayoutManager(this)
 	}
 }
---
+++ app/src/main/java/campus/tech/kakao/contacts/ContactsListAdapter.kt
@@ -0,0 +1,30 @@
+package campus.tech.kakao.contacts
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactsListAdapter(
+	val contactsList: MutableList<ContactData>,
+	val inflater: LayoutInflater
+): RecyclerView.Adapter<ContactsListAdapter.MyViewHolder>() {
+
+	inner class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+		val thumbnail: TextView = itemView.findViewById(R.id.contact_item_thumbnail)
+		val name: TextView = itemView.findViewById(R.id.contact_item_name)
+	}
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+		val view = inflater.inflate(R.layout.item_contact, parent, false)
+		return MyViewHolder(view)
+	}
+
+	override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+		holder.thumbnail.text = contactsList[position].name[0].toString()
+		holder.name.text = contactsList[position].name
+	}
+
+	override fun getItemCount(): Int = contactsList.size
+}
