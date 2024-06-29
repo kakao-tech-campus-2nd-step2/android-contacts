726ead3f6a8be4a68c0135943bce68cd5e0d7294
yb0x00 <yeongbeen0x00@gmail.com>
2024-06-29 23:53:50 +0900

    feat: Add getData() function for data receive
    + findViewById → View binding 변경

--- app/src/main/java/campus/tech/kakao/contacts/ContactListActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/ContactListActivity.kt
@@ -1,11 +1,12 @@
 package campus.tech.kakao.contacts
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,56 +14,78 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import campus.tech.kakao.contacts.databinding.ActivityContactListBinding
 
 class ContactListActivity : AppCompatActivity() {
-
-    lateinit var plusbtn: Button
-    lateinit var infotext : TextView
     val personList = mutableListOf<Person>()
+    lateinit var binding: ActivityContactListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_contact_list)
+
+        binding = ActivityContactListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        setUI()
         goToAddContact()
+    }
+
+    override fun onRestart(){
+        super.onRestart()
+        getData()
+        for (p in personList) {
+            Log.d("ContactListActivity", "Person: ${p.name}, ${p.phone}, ${p.email}, ${p.birth}, ${p.gender}, ${p.note}")
+        }
         displayContact()
     }
 
-    private fun setUI(){
-        plusbtn = findViewById(R.id.btn_plus)
-        infotext = findViewById(R.id.text_info)
+    override fun onDestroy() {
+        super.onDestroy()
+        personList.clear()
     }
 
     fun goToAddContact() {
-        plusbtn.setOnClickListener {
+        binding.btnPlus.setOnClickListener {
             val intent = Intent(this, SaveContactActivity::class.java)
             startActivity(intent)
         }
     }
 
     fun displayContact(){
-        val name = intent.getStringExtra("name")
-        val phone = intent.getStringExtra("phone")
+        Log.d("ContactListActivity","${personList.size}")
+        if (personList.size != 0) {
+            binding.textInfo.visibility = View.GONE
 
-        if (name != null && phone != null) {
-            infotext.visibility = View.GONE
-
-            personList.add(Person(name.toString(), phone))
-
-            with(findViewById<RecyclerView>(R.id.contactRecyclerView)) {
-                this.layoutManager = LinearLayoutManager(this@ContactListActivity)
-                this.adapter = PersonAdapter(
+            with(binding.contactRecyclerView) {
+                layoutManager = LinearLayoutManager(this@ContactListActivity)
+                adapter = PersonAdapter(
                     personList = personList,
                     inflater = LayoutInflater.from(this@ContactListActivity)
                 )
                 adapter?.notifyItemInserted(personList.size - 1)
+            }
+        }
+    }
+
+    private fun getData(){
+        with(getSharedPreferences(CONTACT_INFORMATION, Context.MODE_PRIVATE)){
+            val nameInfo = getString(NAME,null)
+            val phoneInfo = getString(PHONE,null)
+            val emailInfo = getString(EMAIL,null)
+            val birthInfo = getString(BIRTH,null)
+            val genderInfo = getString(GENDER_TYPE,null)
+            val noteInfo = getString(NOTE,null)
+            if(nameInfo != null && phoneInfo != null){
+                val person = Person(nameInfo, phoneInfo, emailInfo, birthInfo, genderInfo, noteInfo)
+                if (!personList.contains(person)) {
+                    personList.add(person)
+                }
             }
         }
     }
@@ -73,12 +96,8 @@ class PersonAdapter(
 ):RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
     class ViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
-        val text_name: TextView
-        val text_initial: TextView
-        init{
-            text_name = itemView.findViewById(R.id.name)
-            text_initial = itemView.findViewById(R.id.nameinitial)
-        }
+        val text_name: TextView = itemView.findViewById(R.id.name)
+        val text_initial: TextView = itemView.findViewById(R.id.nameinitial)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = inflater.inflate(R.layout.activity_person_item, parent, false)
@@ -86,7 +105,7 @@ class PersonAdapter(
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val person = personList.get(position)
+        val person = personList[position]
         val viewHolder = holder as ViewHolder
         viewHolder.text_name.text = person.name
         viewHolder.text_initial.text = person.name[0].toString()
