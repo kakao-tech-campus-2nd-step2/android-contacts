128f905febf862818b8dbacd0388d32dfe345651
leeghy <epuy109@gmail.com>
2024-07-01 01:06:18 +0900

    info 화면 그리기

---
+++ app/src/main/java/campus/tech/kakao/contacts/AddInfo.kt
@@ -0,0 +1,17 @@
+package campus.tech.kakao.contacts
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class AddInfo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_info)
+
+
+    }
+
+}
+
