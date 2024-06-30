4d3b33b19ab7c1642c18ed73c3e472d82777ed88
tlsgptj <angele0709@naver.com>
2024-06-30 16:32:25 +0900

    git commit

--- app/src/main/java/campus/tech/kakao/contacts/whoamI.kt
+++ app/src/main/java/campus/tech/kakao/contacts/whoamI.kt
@@ -11,12 +11,12 @@ class whoamI : AppCompatActivity() {
 
         val contact = intent.getSerializableExtra("contact") as? Contact
         if (contact != null) {
-            findViewById<TextView>(R.id.nameTextView).text = contact.name
-            findViewById<TextView>(R.id.phoneTextView).text = contact.phone
-            findViewById<TextView>(R.id.emailTextView).text = contact.email
-            findViewById<TextView>(R.id.BirTextView).text = contact.birthday
-            findViewById<TextView>(R.id.memoTextView).text = contact.message
-            findViewById<TextView>(R.id.genderTextView).text = contact.gender
+            findViewById<TextView>(R.id.nameTextView).text = contact.contactData.toString()
+            findViewById<TextView>(R.id.phoneTextView).text = contact.contactData.toString()
+            findViewById<TextView>(R.id.emailTextView).text = contact.contactData.toString()
+            findViewById<TextView>(R.id.BirTextView).text = contact.contactData.toString()
+            findViewById<TextView>(R.id.memoTextView).text = contact.contactData.toString()
+            findViewById<TextView>(R.id.genderTextView).text = contact.contactData.toString()
         }
     }
 }
