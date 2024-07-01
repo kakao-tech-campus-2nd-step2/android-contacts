3a51e171823865f59e0c51a7a0eefdb623b3180c
yb0x00 <yeongbeen0x00@gmail.com>
2024-07-01 15:22:34 +0900

    [step2 PR 피드백 반영]refactor: Change var(mutable variables) to val(immutable variables)
    - 피드백을 반영하여 Person이 값이 불변인 VO(value object)이므로 val로 선언

--- app/src/main/java/campus/tech/kakao/contacts/Person.kt
+++ app/src/main/java/campus/tech/kakao/contacts/Person.kt
@@ -1,5 +1,5 @@
 package campus.tech.kakao.contacts
 
-data class Person(var name:String, var phone:String, var email:String?, var birth:String?, var gender:String?, var note:String?) {
+data class Person(val name:String, val phone:String, val email:String?, val birth:String?, val gender:String?, val note:String?) {
 
 }
