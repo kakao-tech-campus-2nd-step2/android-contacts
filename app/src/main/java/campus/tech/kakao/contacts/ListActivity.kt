e8abd1064ed0783a911d512ad8a5645128a4a964
jooiss <132315974+jooiss@users.noreply.github.com>
2024-07-05 01:28:48 +0900

    [부산대 Android_김주송] 1주차 과제 제출합니다. (#36)
    * docs: Updte README.md
    
    * feat: Add logo icon
    
    * feat: Add name input form layout
    
    * feat: Add phone number input form layout
    
    * feat: Add email input form layout
    
    * feat: Add more Button layout
    
    * feat: Add addition information form layout
    
    * feat: Add cancel and save button layout
    
    * feat: Add name requirement
    
    * feat: Add phone number requirement
    
    * feat: Add save button message print function
    
    * feat: Add cancel button message print function
    
    * feat: Add more button function
    
    * feat: Add birthday input form function
    
    * design: Add list layout
    
    * design: Add list item layout
    
    * design: Add list item detail layout
    
    * feat: Add data transmission function of save button
    
    * feat: Add go back function of cancel button
    
    * Design: Add scrollView
    
    * feat: Add Contact data clss
    
    * feat: Add contact item addition function
    
    * feat: Add contact item detail print function
    
    * fix: Change visibility of contact item detail
    
    * fix: Change gender pring

---
+++ app/src/main/java/campus/tech/kakao/contacts/ListActivity.kt
@@ -0,0 +1,87 @@
+package campus.tech.kakao.contacts
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ListActivity : AppCompatActivity() {
+
+    private lateinit var contactLinearView: LinearLayout
+    private val contacts = mutableListOf<Contact>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_layout)
+
+        contactLinearView = findViewById<LinearLayout>(R.id.contactLinearView)
+        val contactAddButton = findViewById<Button>(R.id.contactAddButton)
+
+        contactAddButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_ADD_CONTACT)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_ADD_CONTACT && resultCode == Activity.RESULT_OK) {
+            val name = data?.getStringExtra("name") ?: ""
+            val phone = data?.getStringExtra("phone") ?: ""
+            val email = data?.getStringExtra("email") ?: ""
+            val birthday = data?.getStringExtra("birthday") ?: ""
+            val gender = data?.getStringExtra("gender") ?: ""
+            val memo = data?.getStringExtra("memo") ?: ""
+            val contact = Contact(name, phone, email, birthday, gender, memo)
+            contacts.add(contact)
+            addContactView(contact)
+        }
+    }
+
+    private fun addContactView(contact: Contact) {
+        val inflater = LayoutInflater.from(this)
+        val contactView = inflater.inflate(R.layout.item_layout, contactLinearView, false)
+
+        val tvInitial = contactView.findViewById<TextView>(R.id.itemInitial)
+        val tvName = contactView.findViewById<TextView>(R.id.itemName)
+
+        tvInitial.text = contact.name.first().toString()
+        tvName.text = contact.name
+
+        contactView.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java).apply {
+                putExtra("name", contact.name)
+                putExtra("phone", contact.phone)
+                putExtra("email", contact.email)
+                putExtra("birthday", contact.birthday)
+                putExtra("gender", contact.gender)
+                putExtra("memo", contact.memo)
+            }
+            startActivity(intent)
+        }
+
+        contactView.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java).apply {
+                putExtra("name", contact.name)
+                putExtra("phone", contact.phone)
+                putExtra("email", contact.email)
+                putExtra("birthday", contact.birthday)
+                putExtra("gender", contact.gender)
+                putExtra("memo", contact.memo)
+            }
+            startActivity(intent)
+        }
+
+        contactLinearView.addView(contactView)
+    }
+
+
+
+    companion object {
+        const val REQUEST_CODE_ADD_CONTACT = 1
+    }
+}
--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,81 @@
 package campus.tech.kakao.contacts
 
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nameInputForm = findViewById<EditText>(R.id.nameInputForm)
+        val phoneInputForm = findViewById<EditText>(R.id.phoneInputForm)
+        val emailInputForm = findViewById<EditText>(R.id.emailInputForm)
+        val moreButton = findViewById<LinearLayout>(R.id.moreButton)
+        val moreInformationForm = findViewById<LinearLayout>(R.id.moreInformationForm)
+        val birthdayInputForm = findViewById<TextView>(R.id.birthdayInputForm)
+        val genderInputForm = findViewById<RadioGroup>(R.id.genderRadioGroup)
+        val memoInputForm = findViewById<EditText>(R.id.memoInputForm)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val cancelButton = findViewById<Button>(R.id.cancelButton)
+
+
+        moreButton.setOnClickListener {
+            moreInformationForm.setVisibility(View.VISIBLE)
+            moreButton.setVisibility(View.GONE)
+        }
+
+        birthdayInputForm.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(this,
+                { _, year, monthOfYear, dayOfMonth ->
+                    val formattedMonth = String.format("%02d", monthOfYear + 1)
+                    val formattedDay = String.format("%02d", dayOfMonth)
+                    birthdayInputForm.text = "$year.$formattedMonth.$formattedDay"
+                }, 2000, 0, 1)
+            datePickerDialog.show()
+        }
+
+        saveButton.setOnClickListener {
+            if (nameInputForm.text.toString().trim().isEmpty()) {
+                Toast.makeText(this, "이름은 필수 값 입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else if (phoneInputForm.text.toString().trim().isEmpty()) {
+                Toast.makeText(this, "전화 번호는 필수 값 입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val name = nameInputForm.text.toString()
+                val phone = phoneInputForm.text.toString()
+                val email = emailInputForm.text.toString()
+                val birthday = birthdayInputForm.text.toString()
+                val gender = genderInputForm.checkedRadioButtonId.toString()
+                val memo = memoInputForm.text.toString()
+                val intent = Intent().apply {
+                    putExtra("name", name)
+                    putExtra("phone", phone)
+                    putExtra("email", email)
+                    putExtra("birthday", birthday)
+                    putExtra("gender", gender)
+                    putExtra("memo", memo)
+                }
+                setResult(RESULT_OK, intent)
+                Toast.makeText(this, "저장이 완료 되었습니다.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+
+        cancelButton.setOnClickListener {
+            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
