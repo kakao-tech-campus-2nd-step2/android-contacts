1d495a37fe9e4283c07e3d950dca5a2aaa2546dd
khyeonm <101093716+khyeonm@users.noreply.github.com>
2024-07-05 01:33:25 +0900

    부산대 Android_김현민_1주차_과제 (#11)
    * docs: Update README.md
    
    * feat: add new drawable files
    
    * feat: complete contact save screen
    
    * feat: implement simple input function
    
    * feat: add save function
    
    * feat: Add more function
    
    Step 1 finish
    
    * feat: Design contact list layout
    
    Additionally create required drawable files
    
    * feat: Design contact list item
    
    Additionally create required drawable files
    
    * feat: Design contact detail layout
    
    * feat: Add interaction between ContactList and MainActivity
    
    * feat: Add back button popup function
    
    * feat: Add interaction between Detail and MainActivity
    
    * refactor: Handle empty birthday
    
    * docs: Update README.md
    
    * style: Enhance detailed design
    
    * fix: View birthday in detail

---
+++ app/src/main/java/campus/tech/kakao/contacts/Contact_List.kt
@@ -0,0 +1,83 @@
+package campus.tech.kakao.contacts
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
+
+class Contact_List : AppCompatActivity() {
+    lateinit var contactListLayout: LinearLayoutCompat
+    lateinit var addContact: ActivityResultLauncher<Intent>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_list)
+
+        val add = findViewById<Button>(R.id.add)
+        contactListLayout = findViewById<LinearLayoutCompat>(R.id.contactlist)
+
+        addContact = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data
+                val name = data?.getStringExtra("name")
+                val phone = data?.getStringExtra("phone")
+                val email = data?.getStringExtra("email") ?: ""
+                val birthday = data?.getStringExtra("birthday") ?: ""
+                val gender = data?.getStringExtra("gender") ?: ""
+                val memo = data?.getStringExtra("memo") ?: ""
+                if (name != null && phone != null) {
+                    addContactItem(name, phone, email, birthday, gender, memo)
+                }
+            }
+        }
+
+        add.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            addContact.launch(intent)
+            val notice = findViewById<TextView>(R.id.notice)
+            notice.visibility = View.GONE
+        }
+    }
+
+    fun addContactItem(
+        name: String,
+        phone: String,
+        email: String,
+        birthday: String,
+        gender: String,
+        memo: String
+    ) {
+        val firstname = name[0].toString()
+
+        // 새 연락처 아이템을 생성
+        val contactView =
+            LayoutInflater.from(this).inflate(R.layout.activity_list_item, contactListLayout, false)
+
+        val itemName = contactView.findViewById<TextView>(R.id.itemname)
+        val profileName = contactView.findViewById<TextView>(R.id.profilename)
+
+        itemName.text = name
+        profileName.text = firstname
+
+        contactView.setOnClickListener {
+            val detailIntent = Intent(this, ContactDetail::class.java).apply {
+                putExtra("name", name)
+                putExtra("phone", phone)
+                putExtra("email", email)
+                putExtra("birthday", birthday)
+                putExtra("gender", gender)
+                putExtra("memo", memo)
+            }
+            startActivity(detailIntent)
+        }
+        contactListLayout.addView(contactView)
+
+    }
+}
--- app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/contacts/MainActivity.kt
@@ -1,11 +1,140 @@
 package campus.tech.kakao.contacts
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.constraintlayout.widget.ConstraintLayout
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name = findViewById<EditText>(R.id.name)
+        val phone = findViewById<EditText>(R.id.phone)
+        val mail = findViewById<EditText>(R.id.mail)
+        val save = findViewById<Button>(R.id.save)
+        val cancel = findViewById<Button>(R.id.cancel)
+        val more = findViewById<LinearLayoutCompat>(R.id.more)
+        val birthday = findViewById<TextView>(R.id.birthday)
+        val moreLayout = findViewById<LinearLayoutCompat>(R.id.moreLayout)
+        val birthdayLayout = findViewById<ConstraintLayout>(R.id.birthdayLayout)
+        val genderLayout = findViewById<ConstraintLayout>(R.id.genderLayout)
+        val female = findViewById<RadioButton>(R.id.female)
+        val male = findViewById<RadioButton>(R.id.male)
+        val memo = findViewById<EditText>(R.id.memo)
+
+        name.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        phone.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        mail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        cancel.setOnClickListener {
+            Toast.makeText(this@MainActivity, "취소 되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        save.setOnClickListener {
+            val writename = name.text.toString()
+            val writenumber = phone.text.toString()
+            val writemail = mail.text.toString()
+            val writebir = birthday.text.toString().ifEmpty {""}
+            var writegender = if (female.isChecked) "Female" else if (male.isChecked) "Male" else ""
+            val writememo = memo.text.toString()
+
+            var namecomplete = false
+            var numbercomplete = false
+
+            if (writename.isBlank()) {
+                Toast.makeText(this@MainActivity, "이름은 필수 입력 값입니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                namecomplete = true
+            }
+
+            if (writenumber.isBlank()) {
+                Toast.makeText(this@MainActivity, "전화번호는 필수 입력 값입니다.", Toast.LENGTH_SHORT).show()
+            } else if (!writenumber.all { it.isDigit() }) {
+                Toast.makeText(this@MainActivity, "전화번호에는 숫자만 입력할 수 있습니다.", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                numbercomplete = true
+            }
+
+            if (namecomplete == true && numbercomplete == true) {
+                Toast.makeText(this@MainActivity, "저장이 완료 되었습니다", Toast.LENGTH_SHORT).show()
+            }
+
+            if (writename.isNotEmpty() && writenumber.isNotEmpty() && writenumber.all { it.isDigit() }) {
+                val intent = Intent().apply {
+                    putExtra("name", writename)
+                    putExtra("phone", writenumber)
+                    putExtra("email", writemail)
+                    putExtra("birthday", writebir)
+                    putExtra("gender", writegender)
+                    putExtra("memo", writememo)
+                }
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+        }
+
+        more.setOnClickListener {
+            moreLayout.visibility = View.GONE
+            birthdayLayout.visibility = View.VISIBLE
+            genderLayout.visibility = View.VISIBLE
+            memo.visibility = View.VISIBLE
+
+            birthdayLayout.setOnClickListener {
+                showDatePicker()
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val builder = AlertDialog.Builder(this@MainActivity)
+                builder.setMessage("작성중인 내용이 있습니다. 정말 나가겠습니까?")
+                builder.setPositiveButton("나가기", DialogInterface.OnClickListener { dialog, which ->
+                    finish()
+                })
+                builder.setNegativeButton("작성 재개", null)
+                builder.show()
+            }
+        })
+    }
+    fun showDatePicker() {
+        val birthday = findViewById<TextView>(R.id.birthday)
+        val calendar = Calendar.getInstance()
+        val data = DatePickerDialog.OnDateSetListener { view, year, month, day ->
+            birthday.text = "${year}.${month}.${day}"
+        }
+
+        DatePickerDialog(this, data, calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)).show()
     }
 }
